3972814048cb85265dc02b77442400e2687a39ae
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-12-26 15:01:36 +0100

    Update SkiaUtil.cs

--- src/Svg.Skia/SkiaUtil.cs
+++ src/Svg.Skia/SkiaUtil.cs
@@ -15,6 +15,19 @@ using Svg.Transforms;
 
 namespace Svg.Skia
 {
+    [Flags]
+    internal enum PathPointType : byte
+    {
+        Start = 0,
+        Line = 1,
+        Bezier = 3,
+        Bezier3 = 3,
+        PathTypeMask = 0x7,
+        DashMode = 0x10,
+        PathMarker = 0x20,
+        CloseSubpath = 0x80
+    }
+
     internal static class SkiaUtil
     {
         internal static SKSize GetDimensions(SvgFragment svgFragment)
@@ -321,6 +334,72 @@ namespace Svg.Skia
 
             disposable.Add(skPath);
             return skPath;
+        }
+
+        internal static List<(SKPoint Point, byte Type)> GetPathTypes(SKPath path)
+        {
+            // System.Drawing.Drawing2D.GraphicsPath.PathTypes
+            // System.Drawing.Drawing2D.PathPointType
+            // byte -> PathPointType
+            var pathTypes = new List<(SKPoint Point, byte Type)>();
+            using (var iterator = path.CreateRawIterator())
+            {
+                var points = new SKPoint[4];
+                var pathVerb = SKPathVerb.Move;
+                (SKPoint Point, byte Type) lastPoint = (default, 0);
+                while ((pathVerb = iterator.Next(points)) != SKPathVerb.Done)
+                {
+                    switch (pathVerb)
+                    {
+                        case SKPathVerb.Move:
+                            {
+                                pathTypes.Add((points[0], (byte)PathPointType.Start));
+                                lastPoint = (points[0], (byte)PathPointType.Start);
+                            }
+                            break;
+                        case SKPathVerb.Line:
+                            {
+                                //pathTypes.Add((points[0], (byte)PathPointType.Line));
+                                pathTypes.Add((points[1], (byte)PathPointType.Line));
+                                lastPoint = (points[1], (byte)PathPointType.Line);
+                            }
+                            break;
+                        case SKPathVerb.Cubic:
+                            {
+                                //pathTypes.Add((points[0], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[1], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[2], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[3], (byte)PathPointType.Bezier));
+                                lastPoint = (points[3], (byte)PathPointType.Bezier);
+                            }
+                            break;
+                        case SKPathVerb.Quad:
+                            {
+                                //pathTypes.Add((points[0], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[1], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[2], (byte)PathPointType.Bezier));
+                                lastPoint = (points[2], (byte)PathPointType.Bezier);
+                            }
+                            break;
+                        case SKPathVerb.Conic:
+                            {
+                                //pathTypes.Add((points[0], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[1], (byte)PathPointType.Bezier));
+                                pathTypes.Add((points[2], (byte)PathPointType.Bezier));
+                                lastPoint = (points[2], (byte)PathPointType.Bezier);
+                            }
+                            break;
+                        case SKPathVerb.Close:
+                            {
+                                lastPoint = (lastPoint.Point, (byte)((lastPoint.Type | (byte)PathPointType.CloseSubpath)));
+                                //pathTypes.Add(lastPoint);
+                                pathTypes[pathTypes.Count - 1] = lastPoint;
+                            }
+                            break;
+                    }
+                }
+            }
+            return pathTypes;
         }
 
         internal static float AdjustSvgOpacity(float opacity)
