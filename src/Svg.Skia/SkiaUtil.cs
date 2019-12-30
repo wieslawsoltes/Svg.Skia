4d44d4c9a9ef1c8877e6d9206ceaba6099767a09
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-12-30 19:11:27 +0100

    Update SkiaUtil.cs

--- src/Svg.Skia/SkiaUtil.cs
+++ src/Svg.Skia/SkiaUtil.cs
@@ -820,9 +820,6 @@ namespace Svg.Skia
             {
                 if (patternContentUnits == SvgCoordinateUnits.ObjectBoundingBox)
                 {
-                    var skBoundsTranslateTransform = SKMatrix.MakeTranslation(skBounds.Left, skBounds.Top);
-                    SKMatrix.PreConcat(ref skPictureTransform, ref skBoundsTranslateTransform);
-
                     var skBoundsScaleTransform = SKMatrix.MakeScale(skBounds.Width, skBounds.Height);
                     SKMatrix.PreConcat(ref skPictureTransform, ref skBoundsScaleTransform);
                 }
