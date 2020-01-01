64281c4381e74331644c74d27388bbf7b5d144d8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-01 17:33:33 +0100

    Create Item.cs

---
+++ tests/SvgToPng/ViewModels/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using SkiaSharp;
+using Svg.Skia;
+
+namespace SvgToPng.ViewModels
+{
+    [DataContract]
+    public class Item
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string SvgPath { get; set; }
+
+        [DataMember]
+        public string ReferencePngPath { get; set; }
+
+        [DataMember]
+        public string OutputPngPath { get; set; }
+
+        [IgnoreDataMember]
+        public SKSvg Svg { get; set; }
+
+        [IgnoreDataMember]
+        public SKBitmap ReferencePng { get; set; }
+
+        [IgnoreDataMember]
+        public SKBitmap PixelDiff { get; set; }
+    }
+}
