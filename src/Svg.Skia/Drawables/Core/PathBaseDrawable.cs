e9f8dc1a203374352d755b13f22ca5823570c2ef
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-02 17:24:13 +0100

    Create PathBaseDrawable.cs

---
+++ src/Svg.Skia/Drawables/Core/PathBaseDrawable.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    internal abstract class PathBaseDrawable : BaseDrawable
+    {
+        protected SKPath? skPath;
+        protected SKPath? _skPathClip;
+        protected SKPaint? _skPaintOpacity;
+        protected SKPaint? _skPaintFilter;
+        protected SKPaint? _skPaintFill;
+        protected SKPaint? _skPaintStroke;
+
+        protected override void OnDraw(SKCanvas canvas)
+        {
+            if (!_canDraw)
+            {
+                return;
+            }
+
+            canvas.Save();
+
+            var skMatrixTotal = canvas.TotalMatrix;
+            SKMatrix.PreConcat(ref skMatrixTotal, ref _skMatrix);
+            canvas.SetMatrix(skMatrixTotal);
+
+            if (_skPathClip != null && !_skPathClip.IsEmpty)
+            {
+                canvas.ClipPath(_skPathClip, SKClipOperation.Intersect, _antialias);
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.SaveLayer(_skPaintOpacity);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.SaveLayer(_skPaintFilter);
+            }
+
+            if (_skPaintFill != null)
+            {
+                canvas.DrawPath(skPath, _skPaintFill);
+            }
+
+            if (_skPaintStroke != null)
+            {
+                canvas.DrawPath(skPath, _skPaintStroke);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.Restore();
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.Restore();
+            }
+
+            canvas.Restore();
+        }
+
+        protected override SKRect OnGetBounds()
+        {
+            if (_canDraw)
+            {
+                return _skBounds;
+            }
+            return SKRect.Empty;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _disposable?.Dispose();
+        }
+    }
+}
