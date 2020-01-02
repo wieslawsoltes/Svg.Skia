5855a12fa725a9feb61a3bdfd9f5c737faf30c60
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-02 17:24:41 +0100

    Create PathDrawable.cs

---
+++ src/Svg.Skia/Drawables/PathDrawable.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using System;
+using System.Collections.Generic;
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    internal class PathDrawable : PathBaseDrawable
+    {
+        public PathDrawable(SvgPath svgPath, SKSize sKSize, bool ignoreDisplay)
+        {
+            _ignoreDisplay = ignoreDisplay;
+            _canDraw = CanDraw(svgPath, _ignoreDisplay);
+
+            if (!_canDraw)
+            {
+                return;
+            }
+
+            skPath = SkiaUtil.ToSKPath(svgPath.PathData, svgPath.FillRule, _disposable);
+            if (skPath == null || skPath.IsEmpty)
+            {
+                _canDraw = false;
+                return;
+            }
+
+            _antialias = SkiaUtil.IsAntialias(svgPath);
+
+            _skBounds = skPath.Bounds;
+
+            _skMatrix = SkiaUtil.GetSKMatrix(svgPath.Transforms);
+            _skPathClip = SkiaUtil.GetSvgVisualElementClipPath(svgPath, _skBounds, new HashSet<Uri>(), _disposable);
+            _skPaintOpacity = SkiaUtil.GetOpacitySKPaint(svgPath, _disposable);
+            _skPaintFilter = SkiaUtil.GetFilterSKPaint(svgPath, _disposable);
+
+            if (SkiaUtil.IsValidFill(svgPath))
+            {
+                _skPaintFill = SkiaUtil.GetFillSKPaint(svgPath, sKSize, _skBounds, _disposable);
+            }
+
+            if (SkiaUtil.IsValidStroke(svgPath))
+            {
+                _skPaintStroke = SkiaUtil.GetStrokeSKPaint(svgPath, sKSize, _skBounds, _disposable);
+            }
+        }
+    }
+}
