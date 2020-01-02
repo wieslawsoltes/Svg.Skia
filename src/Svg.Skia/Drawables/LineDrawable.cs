750feb55eb9aad0665043eca86ce96a2d385cf76
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-02 17:24:39 +0100

    Create LineDrawable.cs

---
+++ src/Svg.Skia/Drawables/LineDrawable.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using System;
+using System.Collections.Generic;
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    internal class LineDrawable : PathBaseDrawable
+    {
+        public LineDrawable(SvgLine svgLine, SKSize sKSize, bool ignoreDisplay)
+        {
+            _ignoreDisplay = ignoreDisplay;
+            _canDraw = CanDraw(svgLine, _ignoreDisplay);
+
+            if (!_canDraw)
+            {
+                return;
+            }
+
+            skPath = SkiaUtil.ToSKPath(svgLine, svgLine.FillRule, _disposable);
+            if (skPath == null || skPath.IsEmpty)
+            {
+                _canDraw = false;
+                return;
+            }
+
+            _antialias = SkiaUtil.IsAntialias(svgLine);
+
+            _skBounds = skPath.Bounds;
+
+            _skMatrix = SkiaUtil.GetSKMatrix(svgLine.Transforms);
+            _skPathClip = SkiaUtil.GetSvgVisualElementClipPath(svgLine, _skBounds, new HashSet<Uri>(), _disposable);
+            _skPaintOpacity = SkiaUtil.GetOpacitySKPaint(svgLine, _disposable);
+            _skPaintFilter = SkiaUtil.GetFilterSKPaint(svgLine, _disposable);
+
+            if (SkiaUtil.IsValidFill(svgLine))
+            {
+                _skPaintFill = SkiaUtil.GetFillSKPaint(svgLine, sKSize, _skBounds, _disposable);
+            }
+
+            if (SkiaUtil.IsValidStroke(svgLine))
+            {
+                _skPaintStroke = SkiaUtil.GetStrokeSKPaint(svgLine, sKSize, _skBounds, _disposable);
+            }
+        }
+    }
+}
