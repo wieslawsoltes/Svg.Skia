edc2a1b1e5c9021515d703f2a6dec0e778fba988
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-03 22:30:22 +0100

    Update FragmentDrawable.cs

--- src/Svg.Skia/Drawables/FragmentDrawable.cs
+++ src/Svg.Skia/Drawables/FragmentDrawable.cs
@@ -2,11 +2,128 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 // Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using System;
+using System.Collections.Generic;
+using SkiaSharp;
 
 namespace Svg.Skia
 {
     internal class FragmentDrawable : BaseDrawable
     {
-        // TODO: Implement drawable.
+        internal List<BaseDrawable> _childrenDrawable = new List<BaseDrawable>();
+
+        public FragmentDrawable(SvgFragment svgFragment, SKRect skOwnerBounds, bool ignoreDisplay)
+        {
+            _ignoreDisplay = ignoreDisplay;
+            _canDraw = true;
+
+            float x = svgFragment.X.ToDeviceValue(UnitRenderingType.Horizontal, svgFragment, skOwnerBounds);
+            float y = svgFragment.Y.ToDeviceValue(UnitRenderingType.Vertical, svgFragment, skOwnerBounds);
+            var skSize = SkiaUtil.GetDimensions(svgFragment);
+
+            switch (svgFragment.Overflow)
+            {
+                case SvgOverflow.Auto:
+                case SvgOverflow.Visible:
+                case SvgOverflow.Inherit:
+                    break;
+                default:
+                    _skClipRect = SKRect.Create(x, y, skSize.Width, skSize.Height);
+                    break;
+            }
+
+            foreach (var svgElement in svgFragment.Children)
+            {
+                var drawable = DrawableFactory.Create(svgElement, skOwnerBounds, ignoreDisplay);
+                if (drawable != null)
+                {
+                    _childrenDrawable.Add(drawable);
+                    _disposable.Add(drawable);
+                }
+            }
+
+            _antialias = SkiaUtil.IsAntialias(svgFragment);
+
+            _skBounds = SKRect.Empty;
+
+            foreach (var drawable in _childrenDrawable)
+            {
+                if (_skBounds.IsEmpty)
+                {
+                    _skBounds = drawable._skBounds;
+                }
+                else
+                {
+                    if (!drawable._skBounds.IsEmpty)
+                    {
+                        _skBounds = SKRect.Union(_skBounds, drawable._skBounds);
+                    }
+                }
+            }
+
+            // TODO: Transform _skBounds using _skMatrix.
+
+            _skMatrix = SkiaUtil.GetSKMatrix(svgFragment.Transforms);
+            var skMatrixViewBox = SkiaUtil.GetSvgViewBoxTransform(svgFragment.ViewBox, svgFragment.AspectRatio, x, y, skSize.Width, skSize.Height);
+            SKMatrix.PreConcat(ref _skMatrix, ref skMatrixViewBox);
+
+            _skPathClip = null;
+            _skPaintOpacity = SkiaUtil.GetOpacitySKPaint(svgFragment, _disposable);
+            _skPaintFilter = null;
+
+            _skPaintFill = null;
+            _skPaintStroke = null;
+        }
+
+        protected override void OnDraw(SKCanvas canvas)
+        {
+            if (!_canDraw)
+            {
+                return;
+            }
+
+            canvas.Save();
+
+            if (_skClipRect != null)
+            {
+                canvas.ClipRect(_skClipRect.Value, SKClipOperation.Intersect);
+            }
+
+            var skMatrixTotal = canvas.TotalMatrix;
+            SKMatrix.PreConcat(ref skMatrixTotal, ref _skMatrix);
+            canvas.SetMatrix(skMatrixTotal);
+
+            if (_skPathClip != null && !_skPathClip.IsEmpty)
+            {
+                canvas.ClipPath(_skPathClip, SKClipOperation.Intersect, _antialias);
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.SaveLayer(_skPaintOpacity);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.SaveLayer(_skPaintFilter);
+            }
+
+            foreach (var drawable in _childrenDrawable)
+            {
+                drawable.Draw(canvas, 0f, 0f);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.Restore();
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.Restore();
+            }
+
+            canvas.Restore();
+        }
     }
 }
