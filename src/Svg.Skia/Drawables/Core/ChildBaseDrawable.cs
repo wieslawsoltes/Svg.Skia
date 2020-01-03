5fc9050ca93ce7895259d16d7487604f9bd4b143
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-03 22:53:15 +0100

    Create ChildBaseDrawable.cs

---
+++ src/Svg.Skia/Drawables/Core/ChildBaseDrawable.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using System.Collections.Generic;
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    internal abstract class ChildBaseDrawable : BaseDrawable
+    {
+        internal List<BaseDrawable> _childrenDrawable = new List<BaseDrawable>();
+
+        protected override void OnDraw(SKCanvas canvas)
+        {
+            if (!_canDraw)
+            {
+                return;
+            }
+
+            canvas.Save();
+
+            if (_skClipRect != null)
+            {
+                canvas.ClipRect(_skClipRect.Value, SKClipOperation.Intersect);
+            }
+
+            var skMatrixTotal = canvas.TotalMatrix;
+            SKMatrix.PreConcat(ref skMatrixTotal, ref _skMatrix);
+            canvas.SetMatrix(skMatrixTotal);
+
+            if (_skPathClip != null && !_skPathClip.IsEmpty)
+            {
+                canvas.ClipPath(_skPathClip, SKClipOperation.Intersect, _antialias);
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.SaveLayer(_skPaintOpacity);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.SaveLayer(_skPaintFilter);
+            }
+
+            foreach (var drawable in _childrenDrawable)
+            {
+                drawable.Draw(canvas, 0f, 0f);
+            }
+
+            if (_skPaintFilter != null)
+            {
+                canvas.Restore();
+            }
+
+            if (_skPaintOpacity != null)
+            {
+                canvas.Restore();
+            }
+
+            canvas.Restore();
+        }
+    }
+}
