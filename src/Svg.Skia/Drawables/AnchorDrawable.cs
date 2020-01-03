6bcc6814f3e881a2c8c1a7b665d67f86d20e5f79
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-03 22:53:22 +0100

    Update AnchorDrawable.cs

--- src/Svg.Skia/Drawables/AnchorDrawable.cs
+++ src/Svg.Skia/Drawables/AnchorDrawable.cs
@@ -2,17 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 // Parts of this source file are adapted from the https://github.com/vvvv/SVG
-using System;
-using System.Collections.Generic;
 using SkiaSharp;
 
 namespace Svg.Skia
 {
 #if SVG_ANCHOR
-    internal class AnchorDrawable : BaseDrawable
+    internal class AnchorDrawable : ChildBaseDrawable
     {
-        internal List<BaseDrawable> _childrenDrawable = new List<BaseDrawable>();
-
         public AnchorDrawable(SvgAnchor svgAnchor, SKRect skOwnerBounds, bool ignoreDisplay)
         {
             _ignoreDisplay = ignoreDisplay;
@@ -61,57 +57,6 @@ namespace Svg.Skia
 
             _skPaintFill = null;
             _skPaintStroke = null;
-        }
-
-        protected override void OnDraw(SKCanvas canvas)
-        {
-            if (!_canDraw)
-            {
-                return;
-            }
-
-            canvas.Save();
-
-            if (_skClipRect != null)
-            {
-                canvas.ClipRect(_skClipRect.Value, SKClipOperation.Intersect);
-            }
-
-            var skMatrixTotal = canvas.TotalMatrix;
-            SKMatrix.PreConcat(ref skMatrixTotal, ref _skMatrix);
-            canvas.SetMatrix(skMatrixTotal);
-
-            if (_skPathClip != null && !_skPathClip.IsEmpty)
-            {
-                canvas.ClipPath(_skPathClip, SKClipOperation.Intersect, _antialias);
-            }
-
-            if (_skPaintOpacity != null)
-            {
-                canvas.SaveLayer(_skPaintOpacity);
-            }
-
-            if (_skPaintFilter != null)
-            {
-                canvas.SaveLayer(_skPaintFilter);
-            }
-
-            foreach (var drawable in _childrenDrawable)
-            {
-                drawable.Draw(canvas, 0f, 0f);
-            }
-
-            if (_skPaintFilter != null)
-            {
-                canvas.Restore();
-            }
-
-            if (_skPaintOpacity != null)
-            {
-                canvas.Restore();
-            }
-
-            canvas.Restore();
         }
     }
 #endif
