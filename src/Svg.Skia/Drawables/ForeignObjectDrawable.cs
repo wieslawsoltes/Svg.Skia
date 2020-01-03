bb8810b82d4c157af9a8bc64fc1d3c4a5fcb79ed
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-03 23:54:32 +0100

    Update ForeignObjectDrawable.cs

--- src/Svg.Skia/Drawables/ForeignObjectDrawable.cs
+++ src/Svg.Skia/Drawables/ForeignObjectDrawable.cs
@@ -2,11 +2,15 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 // Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using SkiaSharp;
 
 namespace Svg.Skia
 {
     internal class ForeignObjectDrawable : BaseDrawable
     {
-        // TODO: Implement drawable.
+        public ForeignObjectDrawable(SvgForeignObject svgForeignObject, SKRect skOwnerBounds, bool ignoreDisplay)
+        {
+            // TODO: Implement drawable.
+        }
     }
 }
