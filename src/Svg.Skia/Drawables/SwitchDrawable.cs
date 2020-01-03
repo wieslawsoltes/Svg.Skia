84ad74589b4dceec88c402800ab31f6359c0bb6f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-03 23:54:38 +0100

    Update SwitchDrawable.cs

--- src/Svg.Skia/Drawables/SwitchDrawable.cs
+++ src/Svg.Skia/Drawables/SwitchDrawable.cs
@@ -2,11 +2,15 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 // Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using SkiaSharp;
 
 namespace Svg.Skia
 {
     internal class SwitchDrawable : BaseDrawable
     {
-        // TODO: Implement drawable.
+        public SwitchDrawable(SvgSwitch svgSwitch, SKRect skOwnerBounds, bool ignoreDisplay)
+        {
+            // TODO: Implement drawable.
+        }
     }
 }
