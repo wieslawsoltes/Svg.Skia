1aecb22428c88f5f0c5c0639195e1272be9026ca
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-04 21:49:55 +0100

    Create SkiaExtensions.cs

---
+++ src/Svg.Skia/SkiaExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    public static class SkiaExtensions
+    {
+        public static SKBitmap? ToBitmap(this SKPicture skPicture, SKColor background, float scaleX, float scaleY)
+        {
+            float width = skPicture.CullRect.Width * scaleX;
+            float height = skPicture.CullRect.Height * scaleY;
+            if (width > 0 && height > 0)
+            {
+                var skImageInfo = new SKImageInfo((int)width, (int)height);
+                var skBitmap = new SKBitmap(skImageInfo);
+                using (var skCanvas = new SKCanvas(skBitmap))
+                {
+                    skCanvas.Clear(SKColors.Transparent);
+                    if (background != SKColor.Empty)
+                    {
+                        skCanvas.DrawColor(background);
+                    }
+                    skCanvas.Save();
+                    skCanvas.Scale(scaleX, scaleY);
+                    skCanvas.DrawPicture(skPicture);
+                    skCanvas.Restore();
+                    return skBitmap;
+                }
+            }
+            return null;
+        }
+    }
+}
