b5ddefdd02ecfba1cbecd91e50ef088f1623c43c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-04 21:44:12 +0100

    Update SKSvg.cs

--- src/Svg.Skia/SKSvg.cs
+++ src/Svg.Skia/SKSvg.cs
@@ -7,34 +7,6 @@ using SkiaSharp;
 
 namespace Svg.Skia
 {
-    public static class SKPictureExtensions
-    {
-        public static SKBitmap? ToBitmap(this SKPicture skPicture, SKColor background, float scaleX, float scaleY)
-        {
-            float width = skPicture.CullRect.Width * scaleX;
-            float height = skPicture.CullRect.Height * scaleY;
-            if (width > 0 && height > 0)
-            {
-                var skImageInfo = new SKImageInfo((int)width, (int)height);
-                var skBitmap = new SKBitmap(skImageInfo);
-                using (var skCanvas = new SKCanvas(skBitmap))
-                {
-                    skCanvas.Clear(SKColors.Transparent);
-                    if (background != SKColor.Empty)
-                    {
-                        skCanvas.DrawColor(background);
-                    }
-                    skCanvas.Save();
-                    skCanvas.Scale(scaleX, scaleY);
-                    skCanvas.DrawPicture(skPicture);
-                    skCanvas.Restore();
-                    return skBitmap;
-                }
-            }
-            return null;
-        }
-    }
-
     public class SKSvg : IDisposable
     {
         public static SKDrawable? ToDrawable(SvgElement svgElement, SKRect skBounds, bool ignoreDisplay = false)
