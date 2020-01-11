b3a9b13d32da9f7ee049829aff06bd9e2c3271f3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-11 13:29:37 +0100

    Update MainWindow.xaml.cs

--- samples/AvaloniaSgvImage/MainWindow.xaml.cs
+++ samples/AvaloniaSgvImage/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia;
+﻿using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using Svg.Skia.Avalonia;
 
 namespace AvaloniaSgvImage
 {
@@ -9,6 +12,8 @@ namespace AvaloniaSgvImage
     {
         private readonly Image _svgSourceImage;
         private readonly Image _svgResourceImage;
+        private readonly DockPanel _svgSourceDockPanel;
+        private readonly DockPanel _svgResourceDockPanel;
 
         public MainWindow()
         {
@@ -18,6 +23,11 @@ namespace AvaloniaSgvImage
 #endif
             _svgSourceImage = this.FindControl<Image>("svgSourceImage");
             _svgResourceImage = this.FindControl<Image>("svgResourceImage");
+            _svgSourceDockPanel = this.FindControl<DockPanel>("svgSourceDockPanel");
+            _svgResourceDockPanel = this.FindControl<DockPanel>("svgResourceDockPanel");
+
+            AddHandler(DragDrop.DropEvent, Drop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
         }
 
         private void InitializeComponent()
@@ -42,5 +52,53 @@ namespace AvaloniaSgvImage
                 _svgResourceImage.Stretch = (Stretch)comboxBox.SelectedIndex;
             }
         }
+
+
+        private void DragOver(object sender, DragEventArgs e)
+        {
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+
+            if (!e.Data.Contains(DataFormats.FileNames))
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+
+        private void Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                var fileName = e.Data.GetFileNames()?.FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(fileName))
+                {
+                    if (sender == _svgSourceDockPanel)
+                    {
+                        var svg = new SvgSkia();
+                        var picture = svg.Load(fileName);
+                        if (picture != null)
+                        {
+                            _svgSourceImage.Source = new SvgImage()
+                            {
+                                Source = svg
+                            };
+                        }
+                    }
+
+                    if (sender == _svgResourceDockPanel)
+                    {
+                        var svg = new SvgSkia();
+                        var picture = svg.Load(fileName);
+                        if (picture != null)
+                        {
+                            _svgResourceImage.Source = new SvgImage()
+                            {
+                                Source = svg
+                            };
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
