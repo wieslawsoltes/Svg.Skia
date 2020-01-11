3ecfb8ea35268e8b79e27d16f984d4f6c8380cfa
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-11 12:19:54 +0100

    Refactored

---
+++ src/Svg.Skia.Avalonia/Extensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Avalonia.Markup.Xaml;
+
+namespace Svg.Skia.Avalonia
+{
+    internal static class Extensions
+    {
+        public static T GetService<T>(this IServiceProvider sp) => (T)sp?.GetService(typeof(T))!;
+
+        public static Uri GetContextBaseUri(this IServiceProvider ctx) => ctx.GetService<IUriContext>().BaseUri;
+    }
+}
---
+++ src/Svg.Skia.Avalonia/ISvg.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.ComponentModel;
+using SkiaSharp;
+
+namespace Svg.Skia.Avalonia
+{
+    /// <summary>
+    /// Represents a <see cref="SKPicture"/> image.
+    /// </summary>
+    [TypeConverter(typeof(SvgTypeConverter))]
+    public interface ISvg : IDisposable
+    {
+        /// <summary>
+        /// Gets or sets picture.
+        /// </summary>
+        SKPicture? Picture { get; set; }
+    }
+}
