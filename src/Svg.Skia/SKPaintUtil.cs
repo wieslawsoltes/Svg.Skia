c4cdd8e0d2a615c0df17eb7e07b7955e2db41d9a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-13 21:48:26 +0100

    Update SKPaintUtil.cs

--- src/Svg.Skia/SKPaintUtil.cs
+++ src/Svg.Skia/SKPaintUtil.cs
@@ -634,17 +634,20 @@ namespace Svg.Skia
                     {
                         float value = (string.IsNullOrEmpty(svgColourMatrix.Values) ? 0 : float.Parse(svgColourMatrix.Values, NumberStyles.Any, CultureInfo.InvariantCulture));
                         // TODO: Fix matrix.
+                        var angle = (float)DegreeToRadian(value);
+                        var a1 = Math.Cos(angle);
+                        var a2 = Math.Sin(angle);
                         matrix = new float[]
                         {
-                            (float)(0.213 + Math.Cos(value) * +0.787 + Math.Sin(value) * -0.213),
-                            (float)(0.715 + Math.Cos(value) * -0.715 + Math.Sin(value) * -0.715),
-                            (float)(0.072 + Math.Cos(value) * -0.072 + Math.Sin(value) * +0.928), 0, 0,
-                            (float)(0.213 + Math.Cos(value) * -0.213 + Math.Sin(value) * +0.143),
-                            (float)(0.715 + Math.Cos(value) * +0.285 + Math.Sin(value) * +0.140),
-                            (float)(0.072 + Math.Cos(value) * -0.072 + Math.Sin(value) * -0.283), 0, 0,
-                            (float)(0.213 + Math.Cos(value) * -0.213 + Math.Sin(value) * -0.787),
-                            (float)(0.715 + Math.Cos(value) * -0.715 + Math.Sin(value) * +0.715),
-                            (float)(0.072 + Math.Cos(value) * +0.928 + Math.Sin(value) * +0.072), 0, 0,
+                            (float)(0.213 + a1 * +0.787 + a2 * -0.213),
+                            (float)(0.715 + a1 * -0.715 + a2 * -0.715),
+                            (float)(0.072 + a1 * -0.072 + a2 * +0.928), 0, 0,
+                            (float)(0.213 + a1 * -0.213 + a2 * +0.143),
+                            (float)(0.715 + a1 * +0.285 + a2 * +0.140),
+                            (float)(0.072 + a1 * -0.072 + a2 * -0.283), 0, 0,
+                            (float)(0.213 + a1 * -0.213 + a2 * -0.787),
+                            (float)(0.715 + a1 * -0.715 + a2 * +0.715),
+                            (float)(0.072 + a1 * +0.928 + a2 * +0.072), 0, 0,
                             0, 0, 0, 1, 0
                         };
                     }
