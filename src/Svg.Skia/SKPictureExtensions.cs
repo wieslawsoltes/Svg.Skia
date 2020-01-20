c09de3814d6575246e99379041a44ad07307bfcf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-20 20:14:04 +0100

    Update SKPictureExtensions.cs

--- src/Svg.Skia/SKPictureExtensions.cs
+++ src/Svg.Skia/SKPictureExtensions.cs
@@ -1,11 +1,25 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.IO;
 using SkiaSharp;
 
 namespace Svg.Skia
 {
     public static class SKPictureExtensions
     {
+        public static void Draw(this SKPicture skPicture, SKColor background, float scaleX, float scaleY, SKCanvas skCanvas)
+        {
+            skCanvas.Clear(SKColors.Transparent);
+            if (background != SKColor.Empty)
+            {
+                skCanvas.DrawColor(background);
+            }
+            skCanvas.Save();
+            skCanvas.Scale(scaleX, scaleY);
+            skCanvas.DrawPicture(skPicture);
+            skCanvas.Restore();
+        }
+
         public static SKBitmap? ToBitmap(this SKPicture skPicture, SKColor background, float scaleX, float scaleY)
         {
             float width = skPicture.CullRect.Width * scaleX;
@@ -14,21 +28,77 @@ namespace Svg.Skia
             {
                 var skImageInfo = new SKImageInfo((int)width, (int)height);
                 var skBitmap = new SKBitmap(skImageInfo);
-                using (var skCanvas = new SKCanvas(skBitmap))
-                {
-                    skCanvas.Clear(SKColors.Transparent);
-                    if (background != SKColor.Empty)
-                    {
-                        skCanvas.DrawColor(background);
-                    }
-                    skCanvas.Save();
-                    skCanvas.Scale(scaleX, scaleY);
-                    skCanvas.DrawPicture(skPicture);
-                    skCanvas.Restore();
-                    return skBitmap;
-                }
+                using var skCanvas = new SKCanvas(skBitmap);
+                Draw(skPicture, background, scaleX, scaleY, skCanvas);
+                return skBitmap;
             }
             return null;
+        }
+
+        public static bool ToImage(this SKPicture skPicture, Stream stream, SKColor background, SKEncodedImageFormat format, int quality, float scaleX, float scaleY)
+        {
+            using (var skBitmap = skPicture.ToBitmap(background, scaleX, scaleY))
+            {
+                if (skBitmap == null)
+                {
+                    return false;
+                }
+                using var skImage = SKImage.FromBitmap(skBitmap);
+                using var skData = skImage.Encode(format, quality);
+                if (skData != null)
+                {
+                    skData.SaveTo(stream);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ToSvg(this SKPicture skPicture, string path, SKColor background, float scaleX, float scaleY)
+        {
+            float width = skPicture.CullRect.Width * scaleX;
+            float height = skPicture.CullRect.Height * scaleY;
+            if (width <= 0 || height <= 0)
+            {
+                return false;
+            }
+            using var skFileWStream = new SKFileWStream(path);
+            using var writer = new SKXmlStreamWriter(skFileWStream);
+            using var skCanvas = SKSvgCanvas.Create(SKRect.Create(0, 0, width, height), writer);
+            Draw(skPicture, background, scaleX, scaleY, skCanvas);
+            return true;
+        }
+
+        public static bool ToPdf(this SKPicture skPicture, string path, SKColor background, float scaleX, float scaleY)
+        {
+            float width = skPicture.CullRect.Width * scaleX;
+            float height = skPicture.CullRect.Height * scaleY;
+            if (width <= 0 || height <= 0)
+            {
+                return false;
+            }
+            using var skFileWStream = new SKFileWStream(path);
+            using var skDocument = SKDocument.CreatePdf(skFileWStream, SKDocument.DefaultRasterDpi);
+            using var skCanvas = skDocument.BeginPage(width, height);
+            Draw(skPicture, background, scaleX, scaleY, skCanvas);
+            skDocument.Close();
+            return true;
+        }
+
+        public static bool ToXps(this SKPicture skPicture, string path, SKColor background, float scaleX, float scaleY)
+        {
+            float width = skPicture.CullRect.Width * scaleX;
+            float height = skPicture.CullRect.Height * scaleY;
+            if (width <= 0 || height <= 0)
+            {
+                return false;
+            }
+            using var skFileWStream = new SKFileWStream(path);
+            using var skDocument = SKDocument.CreateXps(skFileWStream, SKDocument.DefaultRasterDpi);
+            using var skCanvas = skDocument.BeginPage(width, height);
+            Draw(skPicture, background, scaleX, scaleY, skCanvas);
+            skDocument.Close();
+            return true;
         }
     }
 }
