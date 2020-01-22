eaeb2a0f287e016a7dd052c07f1108238886de76
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 20:38:18 +0100

    Update MainWindow.xaml.cs

--- tests/SvgToPng/MainWindow.xaml.cs
+++ tests/SvgToPng/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
