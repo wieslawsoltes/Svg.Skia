9860f7c80e48125e83d9ca12486b75105146eafa
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 20:41:32 +0100

    Update SvgFilterskExtensions.cs

--- src/Svg.Skia/SvgFilterskExtensions.cs
+++ src/Svg.Skia/SvgFilterskExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-#define USE_NEW_FILTERS
+//#define USE_NEW_FILTERS
 using System;
 using System.Collections.Generic;
 using System.Globalization;
