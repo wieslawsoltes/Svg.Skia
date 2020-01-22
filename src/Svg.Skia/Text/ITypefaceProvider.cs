e1bc3b8d90f36a03a0a2377ea64f688e48ae2e60
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-01-22 22:28:13 +0100

    Refactored

---
+++ src/Svg.Skia/Text/ITypefaceProvider.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using SkiaSharp;
+
+namespace Svg.Skia
+{
+    public interface ITypefaceProvider
+    {
+        SKTypeface? FromFamilyName(string fontFamily, SKFontStyleWeight fontWeight, SKFontStyleWidth fontWidth, SKFontStyleSlant fontStyle);
+    }
+}
