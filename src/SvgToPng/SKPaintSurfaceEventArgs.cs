e8423b9165aef9badd7c485a091b9f73941d60ca
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-13 21:36:31 +0100

    Create SKPaintSurfaceEventArgs.cs

---
+++ src/SvgToPng/SKPaintSurfaceEventArgs.cs
@@ -0,0 +1,19 @@
+﻿// https://github.com/mono/SkiaSharp/blob/master/source/SkiaSharp.Views/SkiaSharp.Views.Shared/SKPaintSurfaceEventArgs.cs
+using System;
+using SkiaSharp;
+
+namespace SvgToPng
+{
+    public class SKPaintSurfaceEventArgs : EventArgs
+    {
+        public SKPaintSurfaceEventArgs(SKSurface surface, SKImageInfo info)
+        {
+            Surface = surface;
+            Info = info;
+        }
+
+        public SKSurface Surface { get; private set; }
+
+        public SKImageInfo Info { get; private set; }
+    }
+}
