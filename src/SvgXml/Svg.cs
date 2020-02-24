898f4c2b0b5c82ee725bdfeb8822426f36a3e926
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 00:48:16 +0100

    Update Svg.cs

--- src/SvgXml/Svg.cs
+++ src/SvgXml/Svg.cs
@@ -5,6 +5,473 @@ namespace Svg
 {
     public abstract class SvgElement : Element
     {
+        // Conditional processing attributes
+
+        [Attribute("requiredFeatures")]
+        public string? RequiredFeatures
+        {
+            get => GetAttribute("requiredFeatures");
+            set => SetAttribute("requiredFeatures", value);
+        }
+
+        [Attribute("requiredExtensions")]
+        public string? RequiredExtensions
+        {
+            get => GetAttribute("requiredExtensions");
+            set => SetAttribute("requiredExtensions", value);
+        }
+
+        [Attribute("systemLanguage")]
+        public string? SystemLanguage
+        {
+            get => GetAttribute("systemLanguage");
+            set => SetAttribute("systemLanguage", value);
+        }
+
+        // Core attributes
+
+        [Attribute("id")]
+        public string? id
+        {
+            get => GetAttribute("id");
+            set => SetAttribute("id", value);
+        }
+
+        [Attribute("base")]
+        public string? Base
+        {
+            get => GetAttribute("base");
+            set => SetAttribute("base", value);
+        }
+
+        [Attribute("lang")]
+        public string? Lang
+        {
+            get => GetAttribute("lang");
+            set => SetAttribute("lang", value);
+        }
+
+        [Attribute("space")]
+        public string? Space
+        {
+            get => GetAttribute("space");
+            set => SetAttribute("space", value);
+        }
+
+        // Presentation attributes 
+
+        [Attribute("alignment-baseline")]
+        public string? AlignmentBaseline
+        {
+            get => GetAttribute("alignment-baseline");
+            set => SetAttribute("alignment-baseline", value);
+        }
+
+        [Attribute("baseline-shift")]
+        public string? BaselineShift
+        {
+            get => GetAttribute("baseline-shift");
+            set => SetAttribute("baseline-shift", value);
+        }
+
+        [Attribute("clip")]
+        public string? Clip
+        {
+            get => GetAttribute("clip");
+            set => SetAttribute("clip", value);
+        }
+
+        [Attribute("clip-path")]
+        public string? ClipPath
+        {
+            get => GetAttribute("clip-path");
+            set => SetAttribute("clip-path", value);
+        }
+
+        [Attribute("clip-rule")]
+        public string? ClipRule
+        {
+            get => GetAttribute("clip-rule");
+            set => SetAttribute("clip-rule", value);
+        }
+
+        [Attribute("color")]
+        public string? Color
+        {
+            get => GetAttribute("color");
+            set => SetAttribute("color", value);
+        }
+
+        [Attribute("color-interpolation")]
+        public string? ColorInterpolation
+        {
+            get => GetAttribute("color-interpolation");
+            set => SetAttribute("color-interpolation", value);
+        }
+
+        [Attribute("color-interpolation-filters")]
+        public string? ColorInterpolationFilters
+        {
+            get => GetAttribute("color-interpolation-filters");
+            set => SetAttribute("color-interpolation-filters", value);
+        }
+
+        [Attribute("color-profile")]
+        public string? ColorProfile
+        {
+            get => GetAttribute("color-profile");
+            set => SetAttribute("color-profile", value);
+        }
+
+        [Attribute("color-rendering")]
+        public string? ColorRendering
+        {
+            get => GetAttribute("color-rendering");
+            set => SetAttribute("color-rendering", value);
+        }
+
+        [Attribute("cursor")]
+        public string? Cursor
+        {
+            get => GetAttribute("cursor");
+            set => SetAttribute("cursor", value);
+        }
+
+        [Attribute("direction")]
+        public string? Direction
+        {
+            get => GetAttribute("direction");
+            set => SetAttribute("direction", value);
+        }
+
+        [Attribute("display")]
+        public string? Display
+        {
+            get => GetAttribute("display");
+            set => SetAttribute("display", value);
+        }
+
+        [Attribute("dominant-baseline")]
+        public string? DominantBaseline
+        {
+            get => GetAttribute("dominant-baseline");
+            set => SetAttribute("dominant-baseline", value);
+        }
+
+        [Attribute("enable-background")]
+        public string? EnableBackground
+        {
+            get => GetAttribute("enable-background");
+            set => SetAttribute("enable-background", value);
+        }
+
+        [Attribute("fill")]
+        public string? Fill
+        {
+            get => GetAttribute("fill");
+            set => SetAttribute("fill", value);
+        }
+
+        [Attribute("fill-opacity")]
+        public string? FillOpacity
+        {
+            get => GetAttribute("fill-opacity");
+            set => SetAttribute("fill-opacity", value);
+        }
+
+        [Attribute("fill-rule")]
+        public string? FillRule
+        {
+            get => GetAttribute("fill-rule");
+            set => SetAttribute("fill-rule", value);
+        }
+
+        [Attribute("filter")]
+        public string? Filter
+        {
+            get => GetAttribute("filter");
+            set => SetAttribute("filter", value);
+        }
+
+        [Attribute("flood-color")]
+        public string? FloodColor
+        {
+            get => GetAttribute("flood-color");
+            set => SetAttribute("flood-color", value);
+        }
+
+        [Attribute("flood-opacity")]
+        public string? FloodOpacity
+        {
+            get => GetAttribute("flood-opacity");
+            set => SetAttribute("flood-opacity", value);
+        }
+
+        [Attribute("font-family")]
+        public string? FontFamily
+        {
+            get => GetAttribute("font-family");
+            set => SetAttribute("font-family", value);
+        }
+
+        [Attribute("font-size")]
+        public string? FontSize
+        {
+            get => GetAttribute("font-size");
+            set => SetAttribute("font-size", value);
+        }
+
+        [Attribute("font-size-adjust")]
+        public string? FontSizeAdjust
+        {
+            get => GetAttribute("font-size-adjust");
+            set => SetAttribute("font-size-adjust", value);
+        }
+
+        [Attribute("font-stretch")]
+        public string? FontStretch
+        {
+            get => GetAttribute("font-stretch");
+            set => SetAttribute("font-stretch", value);
+        }
+
+        [Attribute("font-style")]
+        public string? FontStyle
+        {
+            get => GetAttribute("font-style");
+            set => SetAttribute("font-style", value);
+        }
+
+        [Attribute("font-variant")]
+        public string? FontVariant
+        {
+            get => GetAttribute("font-variant");
+            set => SetAttribute("font-variant", value);
+        }
+
+        [Attribute("font-weight")]
+        public string? FontWeight
+        {
+            get => GetAttribute("font-weight");
+            set => SetAttribute("font-weight", value);
+        }
+
+        [Attribute("glyph-orientation-horizontal")]
+        public string? GlyphOrientationHorizontal
+        {
+            get => GetAttribute("glyph-orientation-horizontal");
+            set => SetAttribute("glyph-orientation-horizontal", value);
+        }
+
+        [Attribute("glyph-orientation-vertical")]
+        public string? GlyphOrientationVertical
+        {
+            get => GetAttribute("glyph-orientation-vertical");
+            set => SetAttribute("glyph-orientation-vertical", value);
+        }
+
+        [Attribute("image-rendering")]
+        public string? ImageRendering
+        {
+            get => GetAttribute("image-rendering");
+            set => SetAttribute("image-rendering", value);
+        }
+
+        [Attribute("kerning")]
+        public string? Kerning
+        {
+            get => GetAttribute("kerning");
+            set => SetAttribute("kerning", value);
+        }
+
+        [Attribute("letter-spacing")]
+        public string? LetterSpacing
+        {
+            get => GetAttribute("letter-spacing");
+            set => SetAttribute("letter-spacing", value);
+        }
+
+        [Attribute("lighting-color")]
+        public string? LightingColor
+        {
+            get => GetAttribute("lighting-color");
+            set => SetAttribute("lighting-color", value);
+        }
+
+        [Attribute("marker-end")]
+        public string? MarkerEnd
+        {
+            get => GetAttribute("marker-end");
+            set => SetAttribute("marker-end", value);
+        }
+
+        [Attribute("marker-mid")]
+        public string? MarkerMid
+        {
+            get => GetAttribute("marker-mid");
+            set => SetAttribute("marker-mid", value);
+        }
+
+        [Attribute("marker-start")]
+        public string? MarkerStart
+        {
+            get => GetAttribute("marker-start");
+            set => SetAttribute("marker-start", value);
+        }
+
+        [Attribute("mask")]
+        public string? Mask
+        {
+            get => GetAttribute("mask");
+            set => SetAttribute("mask", value);
+        }
+
+        [Attribute("opacity")]
+        public string? Opacity
+        {
+            get => GetAttribute("opacity");
+            set => SetAttribute("opacity", value);
+        }
+
+        [Attribute("overflow")]
+        public string? Overflow
+        {
+            get => GetAttribute("overflow");
+            set => SetAttribute("overflow", value);
+        }
+
+        [Attribute("pointer-events")]
+        public string? PointerEvents
+        {
+            get => GetAttribute("pointer-events");
+            set => SetAttribute("pointer-events", value);
+        }
+
+        [Attribute("shape-rendering")]
+        public string? ShapeRendering
+        {
+            get => GetAttribute("shape-rendering");
+            set => SetAttribute("shape-rendering", value);
+        }
+
+        [Attribute("stop-color")]
+        public string? StopColor
+        {
+            get => GetAttribute("stop-color");
+            set => SetAttribute("stop-color", value);
+        }
+
+        [Attribute("stop-opacity")]
+        public string? StopOpacity
+        {
+            get => GetAttribute("stop-opacity");
+            set => SetAttribute("stop-opacity", value);
+        }
+
+        [Attribute("stroke")]
+        public string? Stroke
+        {
+            get => GetAttribute("stroke");
+            set => SetAttribute("stroke", value);
+        }
+
+        [Attribute("stroke-dasharray")]
+        public string? StrokeDasharray
+        {
+            get => GetAttribute("stroke-dasharray");
+            set => SetAttribute("stroke-dasharray", value);
+        }
+
+        [Attribute("stroke-dashoffset")]
+        public string? StrokeDashoffset
+        {
+            get => GetAttribute("stroke-dashoffset");
+            set => SetAttribute("stroke-dashoffset", value);
+        }
+
+        [Attribute("stroke-linecap")]
+        public string? StrokeLinecap
+        {
+            get => GetAttribute("stroke-linecap");
+            set => SetAttribute("stroke-linecap", value);
+        }
+
+        [Attribute("stroke-linejoin")]
+        public string? SrokeLinejoin
+        {
+            get => GetAttribute("stroke-linejoin");
+            set => SetAttribute("stroke-linejoin", value);
+        }
+
+        [Attribute("stroke-miterlimit")]
+        public string? StrokeMiterlimit
+        {
+            get => GetAttribute("stroke-miterlimit");
+            set => SetAttribute("stroke-miterlimit", value);
+        }
+
+        [Attribute("stroke-opacity")]
+        public string? StrokeOpacity
+        {
+            get => GetAttribute("stroke-opacity");
+            set => SetAttribute("stroke-opacity", value);
+        }
+
+        [Attribute("stroke-width")]
+        public string? StrokeWidth
+        {
+            get => GetAttribute("stroke-width");
+            set => SetAttribute("stroke-width", value);
+        }
+
+        [Attribute("text-anchor")]
+        public string? TextAnchor
+        {
+            get => GetAttribute("text-anchor");
+            set => SetAttribute("text-anchor", value);
+        }
+
+        [Attribute("text-decoration")]
+        public string? TextDecoration
+        {
+            get => GetAttribute("text-decoration");
+            set => SetAttribute("text-decoration", value);
+        }
+
+        [Attribute("text-rendering")]
+        public string? TextRendering
+        {
+            get => GetAttribute("text-rendering");
+            set => SetAttribute("text-rendering", value);
+        }
+
+        [Attribute("unicode-bidi")]
+        public string? UnicodeBidi
+        {
+            get => GetAttribute("unicode-bidi");
+            set => SetAttribute("unicode-bidi", value);
+        }
+
+        [Attribute("visibility")]
+        public string? Visibility
+        {
+            get => GetAttribute("visibility");
+            set => SetAttribute("visibility", value);
+        }
+
+        [Attribute("word-spacing")]
+        public string? WordSpacing
+        {
+            get => GetAttribute("word-spacing");
+            set => SetAttribute("word-spacing", value);
+        }
+
+        [Attribute("writing-mode")]
+        public string? WritingMode
+        {
+            get => GetAttribute("writing-mode");
+            set => SetAttribute("writing-mode", value);
+        }
     }
 
     public class SvgDocument : SvgFragment
