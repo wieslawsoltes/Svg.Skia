792a6ed3a1f267bdc41a71f9c891968963fd21d2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 00:47:56 +0100

    Update Xml.cs

--- src/SvgXml/Xml.cs
+++ src/SvgXml/Xml.cs
@@ -16,6 +16,17 @@ namespace Xml
         }
     }
 
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Event)]
+    public class AttributeAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public AttributeAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
     public interface IElementFactory
     {
         ISet<string> Namespaces { get; }
