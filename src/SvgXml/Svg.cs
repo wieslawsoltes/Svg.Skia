3ae36d06a9fc846fae999e1df3440f906deff121
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-24 22:36:26 +0100

    Update Svg.cs

--- src/SvgXml/Svg.cs
+++ src/SvgXml/Svg.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Svg
+﻿
+namespace Xml
 {
-    // ------------------------------------------------------------------------
-    // Xml
-    // ------------------------------------------------------------------------
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml;
 
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class ElementAttribute : Attribute
@@ -16,6 +15,11 @@ namespace Svg
         {
             Name = name;
         }
+    }
+
+    public interface IElementFactory
+    {
+        public Element Create(string name);
     }
 
     public abstract class Element
@@ -32,11 +36,81 @@ namespace Svg
             Children = new List<Element>();
             Attributes = new Dictionary<string, string>();
         }
+
+        public static Element? Open(XmlReader reader, IElementFactory elementFactory)
+        {
+            var elements = new List<Element>();
+            var stack = new Stack<Element>();
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        {
+                            var name = reader.LocalName;
+                            var element = elementFactory.Create(name);
+
+                            if (reader.MoveToFirstAttribute())
+                            {
+                                do
+                                {
+                                    element.Attributes.Add(reader.Name, reader.Value);
+                                }
+                                while (reader.MoveToNextAttribute());
+                                reader.MoveToElement();
+                            }
+
+                            var nodes = stack.Count > 0 ? stack.Peek().Children : elements;
+                            nodes.Add(element);
+
+                            if (!reader.IsEmptyElement)
+                            {
+                                stack.Push(element);
+                            }
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                }
+            }
+            if (elements.Count == 1)
+            {
+                return elements[0];
+            }
+            return null;
+        }
+
+        public static Element? Open(Stream stream, IElementFactory elementFactory)
+        {
+            var settings = new XmlReaderSettings()
+            {
+                ConformanceLevel = ConformanceLevel.Fragment,
+                IgnoreWhitespace = true,
+                IgnoreComments = true
+            };
+            var reader = XmlReader.Create(stream, settings);
+            var element = Open(reader, elementFactory);
+            return element;
+        }
+
+        public static Element? Open(string path, IElementFactory elementFactory)
+        {
+            using var stream = File.OpenRead(path);
+            return Open(stream, elementFactory);
+        }
     }
 
     public class UnknownElement : Element
     {
     }
+}
+
+namespace Svg
+{
+    using Xml;
 
     // ------------------------------------------------------------------------
     // Svg
@@ -50,9 +124,9 @@ namespace Svg
     {
     }
 
-    public class SvgElementFactory
+    public class SvgElementFactory : IElementFactory
     {
-        public static Element Create(string name)
+        public Element Create(string name)
         {
             return name switch
             {
