950295302dcd14c6ab0465a74b6937bfcf9488c8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 01:42:32 +0100

    Update Svg.cs

--- src/SvgXml/Svg.cs
+++ src/SvgXml/Svg.cs
@@ -3,30 +3,51 @@ using Xml;
 
 namespace Svg
 {
+    public class CoreAttributeAttribute : AttributeAttribute
+    {
+        public CoreAttributeAttribute(string name) : base(name)
+        {
+        }
+    }
+
+    public class PresentationAttributeAttribute : AttributeAttribute
+    {
+        public PresentationAttributeAttribute(string name) : base(name)
+        {
+        }
+    }
+
+    public class ConditionalProcessingAttributeAttribute : AttributeAttribute
+    {
+        public ConditionalProcessingAttributeAttribute(string name) : base(name)
+        {
+        }
+    }
+
     public interface ISvgCoreAttributes : IElement
     {
-        [Attribute("id")]
+        [CoreAttribute("id")]
         public string? id
         {
             get => GetAttribute("id");
             set => SetAttribute("id", value);
         }
 
-        [Attribute("base")]
+        [CoreAttribute("base")]
         public string? Base
         {
             get => GetAttribute("base");
             set => SetAttribute("base", value);
         }
 
-        [Attribute("lang")]
+        [CoreAttribute("lang")]
         public string? Lang
         {
             get => GetAttribute("lang");
             set => SetAttribute("lang", value);
         }
 
-        [Attribute("space")]
+        [CoreAttribute("space")]
         public string? Space
         {
             get => GetAttribute("space");
@@ -36,413 +57,413 @@ namespace Svg
 
     public interface ISvgPresentationAttributes : IElement
     {
-        [Attribute("alignment-baseline")]
+        [PresentationAttribute("alignment-baseline")]
         public string? AlignmentBaseline
         {
             get => GetAttribute("alignment-baseline");
             set => SetAttribute("alignment-baseline", value);
         }
 
-        [Attribute("baseline-shift")]
+        [PresentationAttribute("baseline-shift")]
         public string? BaselineShift
         {
             get => GetAttribute("baseline-shift");
             set => SetAttribute("baseline-shift", value);
         }
 
-        [Attribute("clip")]
+        [PresentationAttribute("clip")]
         public string? Clip
         {
             get => GetAttribute("clip");
             set => SetAttribute("clip", value);
         }
 
-        [Attribute("clip-path")]
+        [PresentationAttribute("clip-path")]
         public string? ClipPath
         {
             get => GetAttribute("clip-path");
             set => SetAttribute("clip-path", value);
         }
 
-        [Attribute("clip-rule")]
+        [PresentationAttribute("clip-rule")]
         public string? ClipRule
         {
             get => GetAttribute("clip-rule");
             set => SetAttribute("clip-rule", value);
         }
 
-        [Attribute("color")]
+        [PresentationAttribute("color")]
         public string? Color
         {
             get => GetAttribute("color");
             set => SetAttribute("color", value);
         }
 
-        [Attribute("color-interpolation")]
+        [PresentationAttribute("color-interpolation")]
         public string? ColorInterpolation
         {
             get => GetAttribute("color-interpolation");
             set => SetAttribute("color-interpolation", value);
         }
 
-        [Attribute("color-interpolation-filters")]
+        [PresentationAttribute("color-interpolation-filters")]
         public string? ColorInterpolationFilters
         {
             get => GetAttribute("color-interpolation-filters");
             set => SetAttribute("color-interpolation-filters", value);
         }
 
-        [Attribute("color-profile")]
+        [PresentationAttribute("color-profile")]
         public string? ColorProfile
         {
             get => GetAttribute("color-profile");
             set => SetAttribute("color-profile", value);
         }
 
-        [Attribute("color-rendering")]
+        [PresentationAttribute("color-rendering")]
         public string? ColorRendering
         {
             get => GetAttribute("color-rendering");
             set => SetAttribute("color-rendering", value);
         }
 
-        [Attribute("cursor")]
+        [PresentationAttribute("cursor")]
         public string? Cursor
         {
             get => GetAttribute("cursor");
             set => SetAttribute("cursor", value);
         }
 
-        [Attribute("direction")]
+        [PresentationAttribute("direction")]
         public string? Direction
         {
             get => GetAttribute("direction");
             set => SetAttribute("direction", value);
         }
 
-        [Attribute("display")]
+        [PresentationAttribute("display")]
         public string? Display
         {
             get => GetAttribute("display");
             set => SetAttribute("display", value);
         }
 
-        [Attribute("dominant-baseline")]
+        [PresentationAttribute("dominant-baseline")]
         public string? DominantBaseline
         {
             get => GetAttribute("dominant-baseline");
             set => SetAttribute("dominant-baseline", value);
         }
 
-        [Attribute("enable-background")]
+        [PresentationAttribute("enable-background")]
         public string? EnableBackground
         {
             get => GetAttribute("enable-background");
             set => SetAttribute("enable-background", value);
         }
 
-        [Attribute("fill")]
+        [PresentationAttribute("fill")]
         public string? Fill
         {
             get => GetAttribute("fill");
             set => SetAttribute("fill", value);
         }
 
-        [Attribute("fill-opacity")]
+        [PresentationAttribute("fill-opacity")]
         public string? FillOpacity
         {
             get => GetAttribute("fill-opacity");
             set => SetAttribute("fill-opacity", value);
         }
 
-        [Attribute("fill-rule")]
+        [PresentationAttribute("fill-rule")]
         public string? FillRule
         {
             get => GetAttribute("fill-rule");
             set => SetAttribute("fill-rule", value);
         }
 
-        [Attribute("filter")]
+        [PresentationAttribute("filter")]
         public string? Filter
         {
             get => GetAttribute("filter");
             set => SetAttribute("filter", value);
         }
 
-        [Attribute("flood-color")]
+        [PresentationAttribute("flood-color")]
         public string? FloodColor
         {
             get => GetAttribute("flood-color");
             set => SetAttribute("flood-color", value);
         }
 
-        [Attribute("flood-opacity")]
+        [PresentationAttribute("flood-opacity")]
         public string? FloodOpacity
         {
             get => GetAttribute("flood-opacity");
             set => SetAttribute("flood-opacity", value);
         }
 
-        [Attribute("font-family")]
+        [PresentationAttribute("font-family")]
         public string? FontFamily
         {
             get => GetAttribute("font-family");
             set => SetAttribute("font-family", value);
         }
 
-        [Attribute("font-size")]
+        [PresentationAttribute("font-size")]
         public string? FontSize
         {
             get => GetAttribute("font-size");
             set => SetAttribute("font-size", value);
         }
 
-        [Attribute("font-size-adjust")]
+        [PresentationAttribute("font-size-adjust")]
         public string? FontSizeAdjust
         {
             get => GetAttribute("font-size-adjust");
             set => SetAttribute("font-size-adjust", value);
         }
 
-        [Attribute("font-stretch")]
+        [PresentationAttribute("font-stretch")]
         public string? FontStretch
         {
             get => GetAttribute("font-stretch");
             set => SetAttribute("font-stretch", value);
         }
 
-        [Attribute("font-style")]
+        [PresentationAttribute("font-style")]
         public string? FontStyle
         {
             get => GetAttribute("font-style");
             set => SetAttribute("font-style", value);
         }
 
-        [Attribute("font-variant")]
+        [PresentationAttribute("font-variant")]
         public string? FontVariant
         {
             get => GetAttribute("font-variant");
             set => SetAttribute("font-variant", value);
         }
 
-        [Attribute("font-weight")]
+        [PresentationAttribute("font-weight")]
         public string? FontWeight
         {
             get => GetAttribute("font-weight");
             set => SetAttribute("font-weight", value);
         }
 
-        [Attribute("glyph-orientation-horizontal")]
+        [PresentationAttribute("glyph-orientation-horizontal")]
         public string? GlyphOrientationHorizontal
         {
             get => GetAttribute("glyph-orientation-horizontal");
             set => SetAttribute("glyph-orientation-horizontal", value);
         }
 
-        [Attribute("glyph-orientation-vertical")]
+        [PresentationAttribute("glyph-orientation-vertical")]
         public string? GlyphOrientationVertical
         {
             get => GetAttribute("glyph-orientation-vertical");
             set => SetAttribute("glyph-orientation-vertical", value);
         }
 
-        [Attribute("image-rendering")]
+        [PresentationAttribute("image-rendering")]
         public string? ImageRendering
         {
             get => GetAttribute("image-rendering");
             set => SetAttribute("image-rendering", value);
         }
 
-        [Attribute("kerning")]
+        [PresentationAttribute("kerning")]
         public string? Kerning
         {
             get => GetAttribute("kerning");
             set => SetAttribute("kerning", value);
         }
 
-        [Attribute("letter-spacing")]
+        [PresentationAttribute("letter-spacing")]
         public string? LetterSpacing
         {
             get => GetAttribute("letter-spacing");
             set => SetAttribute("letter-spacing", value);
         }
 
-        [Attribute("lighting-color")]
+        [PresentationAttribute("lighting-color")]
         public string? LightingColor
         {
             get => GetAttribute("lighting-color");
             set => SetAttribute("lighting-color", value);
         }
 
-        [Attribute("marker-end")]
+        [PresentationAttribute("marker-end")]
         public string? MarkerEnd
         {
             get => GetAttribute("marker-end");
             set => SetAttribute("marker-end", value);
         }
 
-        [Attribute("marker-mid")]
+        [PresentationAttribute("marker-mid")]
         public string? MarkerMid
         {
             get => GetAttribute("marker-mid");
             set => SetAttribute("marker-mid", value);
         }
 
-        [Attribute("marker-start")]
+        [PresentationAttribute("marker-start")]
         public string? MarkerStart
         {
             get => GetAttribute("marker-start");
             set => SetAttribute("marker-start", value);
         }
 
-        [Attribute("mask")]
+        [PresentationAttribute("mask")]
         public string? Mask
         {
             get => GetAttribute("mask");
             set => SetAttribute("mask", value);
         }
 
-        [Attribute("opacity")]
+        [PresentationAttribute("opacity")]
         public string? Opacity
         {
             get => GetAttribute("opacity");
             set => SetAttribute("opacity", value);
         }
 
-        [Attribute("overflow")]
+        [PresentationAttribute("overflow")]
         public string? Overflow
         {
             get => GetAttribute("overflow");
             set => SetAttribute("overflow", value);
         }
 
-        [Attribute("pointer-events")]
+        [PresentationAttribute("pointer-events")]
         public string? PointerEvents
         {
             get => GetAttribute("pointer-events");
             set => SetAttribute("pointer-events", value);
         }
 
-        [Attribute("shape-rendering")]
+        [PresentationAttribute("shape-rendering")]
         public string? ShapeRendering
         {
             get => GetAttribute("shape-rendering");
             set => SetAttribute("shape-rendering", value);
         }
 
-        [Attribute("stop-color")]
+        [PresentationAttribute("stop-color")]
         public string? StopColor
         {
             get => GetAttribute("stop-color");
             set => SetAttribute("stop-color", value);
         }
 
-        [Attribute("stop-opacity")]
+        [PresentationAttribute("stop-opacity")]
         public string? StopOpacity
         {
             get => GetAttribute("stop-opacity");
             set => SetAttribute("stop-opacity", value);
         }
 
-        [Attribute("stroke")]
+        [PresentationAttribute("stroke")]
         public string? Stroke
         {
             get => GetAttribute("stroke");
             set => SetAttribute("stroke", value);
         }
 
-        [Attribute("stroke-dasharray")]
+        [PresentationAttribute("stroke-dasharray")]
         public string? StrokeDasharray
         {
             get => GetAttribute("stroke-dasharray");
             set => SetAttribute("stroke-dasharray", value);
         }
 
-        [Attribute("stroke-dashoffset")]
+        [PresentationAttribute("stroke-dashoffset")]
         public string? StrokeDashoffset
         {
             get => GetAttribute("stroke-dashoffset");
             set => SetAttribute("stroke-dashoffset", value);
         }
 
-        [Attribute("stroke-linecap")]
+        [PresentationAttribute("stroke-linecap")]
         public string? StrokeLinecap
         {
             get => GetAttribute("stroke-linecap");
             set => SetAttribute("stroke-linecap", value);
         }
 
-        [Attribute("stroke-linejoin")]
+        [PresentationAttribute("stroke-linejoin")]
         public string? SrokeLinejoin
         {
             get => GetAttribute("stroke-linejoin");
             set => SetAttribute("stroke-linejoin", value);
         }
 
-        [Attribute("stroke-miterlimit")]
+        [PresentationAttribute("stroke-miterlimit")]
         public string? StrokeMiterlimit
         {
             get => GetAttribute("stroke-miterlimit");
             set => SetAttribute("stroke-miterlimit", value);
         }
 
-        [Attribute("stroke-opacity")]
+        [PresentationAttribute("stroke-opacity")]
         public string? StrokeOpacity
         {
             get => GetAttribute("stroke-opacity");
             set => SetAttribute("stroke-opacity", value);
         }
 
-        [Attribute("stroke-width")]
+        [PresentationAttribute("stroke-width")]
         public string? StrokeWidth
         {
             get => GetAttribute("stroke-width");
             set => SetAttribute("stroke-width", value);
         }
 
-        [Attribute("text-anchor")]
+        [PresentationAttribute("text-anchor")]
         public string? TextAnchor
         {
             get => GetAttribute("text-anchor");
             set => SetAttribute("text-anchor", value);
         }
 
-        [Attribute("text-decoration")]
+        [PresentationAttribute("text-decoration")]
         public string? TextDecoration
         {
             get => GetAttribute("text-decoration");
             set => SetAttribute("text-decoration", value);
         }
 
-        [Attribute("text-rendering")]
+        [PresentationAttribute("text-rendering")]
         public string? TextRendering
         {
             get => GetAttribute("text-rendering");
             set => SetAttribute("text-rendering", value);
         }
 
-        [Attribute("unicode-bidi")]
+        [PresentationAttribute("unicode-bidi")]
         public string? UnicodeBidi
         {
             get => GetAttribute("unicode-bidi");
             set => SetAttribute("unicode-bidi", value);
         }
 
-        [Attribute("visibility")]
+        [PresentationAttribute("visibility")]
         public string? Visibility
         {
             get => GetAttribute("visibility");
             set => SetAttribute("visibility", value);
         }
 
-        [Attribute("word-spacing")]
+        [PresentationAttribute("word-spacing")]
         public string? WordSpacing
         {
             get => GetAttribute("word-spacing");
             set => SetAttribute("word-spacing", value);
         }
 
-        [Attribute("writing-mode")]
+        [PresentationAttribute("writing-mode")]
         public string? WritingMode
         {
             get => GetAttribute("writing-mode");
@@ -452,21 +473,21 @@ namespace Svg
 
     public interface ISvgConditionalProcessingAttributes : IElement
     {
-        [Attribute("requiredFeatures")]
+        [ConditionalProcessingAttribute("requiredFeatures")]
         public string? RequiredFeatures
         {
             get => GetAttribute("requiredFeatures");
             set => SetAttribute("requiredFeatures", value);
         }
 
-        [Attribute("requiredExtensions")]
+        [ConditionalProcessingAttribute("requiredExtensions")]
         public string? RequiredExtensions
         {
             get => GetAttribute("requiredExtensions");
             set => SetAttribute("requiredExtensions", value);
         }
 
-        [Attribute("systemLanguage")]
+        [ConditionalProcessingAttribute("systemLanguage")]
         public string? SystemLanguage
         {
             get => GetAttribute("systemLanguage");
