752b32a82eb5f980871809c518a2763f73431a32
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 23:03:00 +0100

    Update SvgSwitch.cs

--- src/SvgXml/Svg/Document Structure/SvgSwitch.cs
+++ src/SvgXml/Svg/Document Structure/SvgSwitch.cs
@@ -5,10 +5,5 @@ namespace Svg
     [Element("switch")]
     public class SvgSwitch : SvgVisualElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgTransformableAttributes
     {
-        public override void Print(string indent)
-        {
-            base.Print(indent);
-            // TODO:
-        }
     }
 }
