05af902e2f45b81a030a6654b40ee2a5c7d6c83e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 23:03:06 +0100

    Update SvgSymbol.cs

--- src/SvgXml/Svg/Document Structure/SvgSymbol.cs
+++ src/SvgXml/Svg/Document Structure/SvgSymbol.cs
@@ -1,14 +1,37 @@
-﻿using Xml;
+﻿using System;
+using Xml;
 
 namespace Svg
 {
     [Element("symbol")]
     public class SvgSymbol : SvgVisualElement, ISvgPresentationAttributes, ISvgStylableAttributes
     {
+        [Attribute("viewBox")]
+        public string? ViewBox
+        {
+            get => GetAttribute("viewBox");
+            set => SetAttribute("viewBox", value);
+        }
+
+        [Attribute("preserveAspectRatio")]
+        public string? AspectRatio
+        {
+            get => GetAttribute("preserveAspectRatio");
+            set => SetAttribute("preserveAspectRatio", value);
+        }
+
         public override void Print(string indent)
         {
             base.Print(indent);
-            // TODO:
+
+            if (ViewBox != null)
+            {
+                Console.WriteLine($"{indent}{nameof(ViewBox)}='{ViewBox}'");
+            }
+            if (AspectRatio != null)
+            {
+                Console.WriteLine($"{indent}{nameof(AspectRatio)}='{AspectRatio}'");
+            }
         }
     }
 }
