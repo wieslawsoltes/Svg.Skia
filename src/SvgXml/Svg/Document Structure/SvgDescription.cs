aa7ef5fb3c77b22fb25758a04c4b00f7250e8aaa
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 23:02:38 +0100

    Update SvgDescription.cs

--- src/SvgXml/Svg/Document Structure/SvgDescription.cs
+++ src/SvgXml/Svg/Document Structure/SvgDescription.cs
@@ -5,10 +5,5 @@ namespace Svg
     [Element("desc")]
     public class SvgDescription : SvgElement, ISvgStylableAttributes
     {
-        public override void Print(string indent)
-        {
-            base.Print(indent);
-            // TODO:
-        }
     }
 }
