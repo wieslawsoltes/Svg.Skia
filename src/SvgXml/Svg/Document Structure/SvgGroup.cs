caf18b0eef254384e02f5848f736707e8b2a79ca
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 23:02:53 +0100

    Update SvgGroup.cs

--- src/SvgXml/Svg/Document Structure/SvgGroup.cs
+++ src/SvgXml/Svg/Document Structure/SvgGroup.cs
@@ -5,10 +5,5 @@ namespace Svg
     [Element("g")]
     public class SvgGroup : SvgMarkerElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgTransformableAttributes
     {
-        public override void Print(string indent)
-        {
-            base.Print(indent);
-            // TODO:
-        }
     }
 }
