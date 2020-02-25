8fbde8a60c14bfbab29e31d450998419a9401af3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 20:15:51 +0100

    Update SvgMask.cs

--- src/SvgXml/Svg/Clipping and Masking/SvgMask.cs
+++ src/SvgXml/Svg/Clipping and Masking/SvgMask.cs
@@ -1,14 +1,81 @@
-﻿using Xml;
+﻿using System;
+using Xml;
 
 namespace Svg
 {
     [Element("mask")]
     public class SvgMask : SvgElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes
     {
+        [Attribute("x")]
+        public string? X
+        {
+            get => GetAttribute("x");
+            set => SetAttribute("x", value);
+        }
+
+        [Attribute("y")]
+        public string? Y
+        {
+            get => GetAttribute("y");
+            set => SetAttribute("y", value);
+        }
+
+        [Attribute("width")]
+        public string? Width
+        {
+            get => GetAttribute("width");
+            set => SetAttribute("width", value);
+        }
+
+        [Attribute("height")]
+        public string? Height
+        {
+            get => GetAttribute("height");
+            set => SetAttribute("height", value);
+        }
+
+        [Attribute("maskUnits")]
+        public string? MaskUnits
+        {
+            get => GetAttribute("maskUnits");
+            set => SetAttribute("maskUnits", value);
+        }
+
+        [Attribute("maskContentUnits")]
+        public string? MaskContentUnits
+        {
+            get => GetAttribute("maskContentUnits");
+            set => SetAttribute("maskContentUnits", value);
+        }
+
         public override void Print(string indent)
         {
             base.Print(indent);
-            // TODO:
+
+            if (X != null)
+            {
+                Console.WriteLine($"{indent}{nameof(X)}='{X}'");
+            }
+            if (Y != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Y)}='{Y}'");
+            }
+            if (Width != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Width)}='{Width}'");
+            }
+            if (Height != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Height)}='{Height}'");
+            }
+            if (MaskUnits != null)
+            {
+                Console.WriteLine($"{indent}{nameof(MaskUnits)}='{MaskUnits}'");
+            }
+            if (MaskContentUnits != null)
+            {
+                Console.WriteLine($"{indent}{nameof(MaskContentUnits)}='{MaskContentUnits}'");
+            }
         }
     }
 }
