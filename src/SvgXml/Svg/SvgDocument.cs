304b1e84e3e153b569ab4b9cfa6e1d17ada6a1ba
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 23:07:27 +0100

    Update SvgDocument.cs

--- src/SvgXml/Svg/SvgDocument.cs
+++ src/SvgXml/Svg/SvgDocument.cs
@@ -2,10 +2,5 @@
 {
     public class SvgDocument : SvgFragment
     {
-        public override void Print(string indent)
-        {
-            base.Print(indent);
-            // TODO:
-        }
     }
 }
