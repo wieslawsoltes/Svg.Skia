252f1a312f82839fb9ad78ff23e847dd307d98b6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-25 20:17:13 +0100

    Update SvgDeferredPaintServer.cs

--- src/SvgXml/Svg/Painting/SvgDeferredPaintServer.cs
+++ src/SvgXml/Svg/Painting/SvgDeferredPaintServer.cs
@@ -2,10 +2,5 @@
 {
     public class SvgDeferredPaintServer : SvgPaintServer
     {
-        public override void Print(string indent)
-        {
-            base.Print(indent);
-            // TODO:
-        }
     }
 }
