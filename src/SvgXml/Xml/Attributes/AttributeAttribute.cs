816b2240a92257404d6d0062d1a9efca25f44fe6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 22:24:28 +0100

    Update AttributeAttribute.cs

--- src/SvgXml/Xml/Attributes/AttributeAttribute.cs
+++ src/SvgXml/Xml/Attributes/AttributeAttribute.cs
@@ -6,10 +6,12 @@ namespace Xml
     public class AttributeAttribute : Attribute
     {
         public string Name { get; private set; }
+        public string NameSpace { get; private set; }
 
-        public AttributeAttribute(string name)
+        public AttributeAttribute(string name, string nameSpace)
         {
             Name = name;
+            NameSpace = nameSpace;
         }
     }
 }
