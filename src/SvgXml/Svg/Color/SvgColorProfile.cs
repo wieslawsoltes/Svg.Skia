4620b8f89c8d224198a98565e5b59ae8b5708528
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 20:09:20 +0100

    Update SvgColorProfile.cs

--- src/SvgXml/Svg/Color/SvgColorProfile.cs
+++ src/SvgXml/Svg/Color/SvgColorProfile.cs
@@ -6,10 +6,54 @@ namespace Svg
     [Element("color-profile")]
     public class SvgColorProfile : SvgElement
     {
+        [Attribute("local")]
+        public string? Local
+        {
+            get => GetAttribute("local");
+            set => SetAttribute("local", value);
+        }
+
+        [Attribute("name")]
+        public string? Name
+        {
+            get => GetAttribute("name");
+            set => SetAttribute("name", value);
+        }
+
+        [Attribute("rendering-intent")]
+        public string? RenderingIntent
+        {
+            get => GetAttribute("rendering-intent");
+            set => SetAttribute("rendering-intent", value);
+        }
+
+        [Attribute("href")]
+        public string? Href
+        {
+            get => GetAttribute("href");
+            set => SetAttribute("href", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (Local != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Local)}='{Local}'");
+            }
+            if (Name != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Name)}='{Name}'");
+            }
+            if (RenderingIntent != null)
+            {
+                Console.WriteLine($"{indent}{nameof(RenderingIntent)}='{RenderingIntent}'");
+            }
+            if (Href != null)
+            {
+                write($"{indent}{nameof(Href)}: \"{Href}\"");
+            }
         }
     }
 }
