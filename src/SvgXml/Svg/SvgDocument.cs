57a0db5bec63dd99747cb25fa1d132ef52523961
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-02-26 08:48:47 +0000

    Updated

--- src/SvgXml/Svg/SvgDocument.cs
+++ src/SvgXml/Svg/SvgDocument.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.IO.Compression;
 using System.Xml;
 using Xml;
 
@@ -28,9 +29,53 @@ namespace Svg
             return null;
         }
 
-        public static SvgDocument? Open(string path)
+        public static SvgDocument? OpenSvg(string path)
         {
             var element = XmlLoader.Open(path, s_elementFactory);
+            if (element is SvgDocument svgDocument)
+            {
+                return svgDocument;
+            }
+            return null;
+        }
+
+        public static SvgDocument? OpenSvgz(string path)
+        {
+            using var fileStream = File.OpenRead(path);
+            using var gzipStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            using var memoryStream = new MemoryStream();
+
+            gzipStream.CopyTo(memoryStream);
+            memoryStream.Position = 0;
+
+            var element = XmlLoader.Open(memoryStream, s_elementFactory);
+            if (element is SvgDocument svgDocument)
+            {
+                return svgDocument;
+            }
+            return null;
+        }
+
+        public static SvgDocument? Open(string path)
+        {
+            var extension = Path.GetExtension(path);
+            return extension.ToLower() switch
+            {
+                ".svg" => OpenSvg(path),
+                ".svgz" => OpenSvgz(path),
+                _ => OpenSvg(path),
+            };
+        }
+
+        public static SvgDocument? FromSvg(string svg)
+        {
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream);
+            streamWriter.Write(svg);
+            streamWriter.Flush();
+            memoryStream.Position = 0;
+
+            var element = XmlLoader.Open(memoryStream, s_elementFactory);
             if (element is SvgDocument svgDocument)
             {
                 return svgDocument;
