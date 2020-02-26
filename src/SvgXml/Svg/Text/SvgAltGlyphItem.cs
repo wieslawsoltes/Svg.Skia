a1872a4592f3c469ba1217d0a0b77d9e35c28d0c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 00:56:12 +0100

    Update SvgAltGlyphItem.cs

--- src/SvgXml/Svg/Text/SvgAltGlyphItem.cs
+++ src/SvgXml/Svg/Text/SvgAltGlyphItem.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using Xml;
+﻿using Xml;
 
 namespace Svg
 {
     [Element("altGlyphItem")]
     public class SvgAltGlyphItem : SvgElement
     {
-        public override void Print(Action<string> write, string indent)
-        {
-            base.Print(write, indent);
-            // TODO:
-        }
     }
 }
