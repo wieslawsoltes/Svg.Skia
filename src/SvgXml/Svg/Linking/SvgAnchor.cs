1bb7f176450ca87b7483b7af86ff756d2ce337b1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 22:25:24 +0100

    Update SvgAnchor.cs

--- src/SvgXml/Svg/Linking/SvgAnchor.cs
+++ src/SvgXml/Svg/Linking/SvgAnchor.cs
@@ -6,10 +6,21 @@ namespace Svg
     [Element("a")]
     public class SvgAnchor : SvgElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgResourcesAttributes, ISvgTransformableAttributes
     {
+        [Attribute("target", SvgAttributes.SvgNamespace)]
+        public string? Target
+        {
+            get => GetAttribute("target");
+            set => SetAttribute("target", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (Target != null)
+            {
+                write($"{indent}{nameof(Target)}='{Target}'");
+            }
         }
     }
 }
