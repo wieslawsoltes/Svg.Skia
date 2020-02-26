62aa858421611672e4517b72452964d7f5527bad
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 00:56:17 +0100

    Update SvgFont.cs

--- src/SvgXml/Svg/Text/SvgFont.cs
+++ src/SvgXml/Svg/Text/SvgFont.cs
@@ -6,10 +6,76 @@ namespace Svg
     [Element("font")]
     public class SvgFont : SvgElement, ISvgPresentationAttributes, ISvgStylableAttributes, ISvgResourcesAttributes
     {
+        [Attribute("horiz-origin-x", SvgAttributes.SvgNamespace)]
+        public string? HorizOriginX
+        {
+            get => GetAttribute("horiz-origin-x");
+            set => SetAttribute("horiz-origin-x", value);
+        }
+
+        [Attribute("horiz-origin-y", SvgAttributes.SvgNamespace)]
+        public string? HorizOriginY
+        {
+            get => GetAttribute("horiz-origin-y");
+            set => SetAttribute("horiz-origin-y", value);
+        }
+
+        [Attribute("horiz-adv-x", SvgAttributes.SvgNamespace)]
+        public string? HorizAdvX
+        {
+            get => GetAttribute("horiz-adv-x");
+            set => SetAttribute("horiz-adv-x", value);
+        }
+
+        [Attribute("vert-origin-x", SvgAttributes.SvgNamespace)]
+        public string? VertOriginX
+        {
+            get => GetAttribute("vert-origin-x");
+            set => SetAttribute("vert-origin-x", value);
+        }
+
+        [Attribute("vert-origin-y", SvgAttributes.SvgNamespace)]
+        public string? VertOriginY
+        {
+            get => GetAttribute("vert-origin-y");
+            set => SetAttribute("vert-origin-y", value);
+        }
+
+        [Attribute("vert-adv-y", SvgAttributes.SvgNamespace)]
+        public string? VertAdvY
+        {
+            get => GetAttribute("vert-adv-y");
+            set => SetAttribute("vert-adv-y", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (HorizOriginX != null)
+            {
+                write($"{indent}{nameof(HorizOriginX)}: \"{HorizOriginX}\"");
+            }
+            if (HorizOriginY != null)
+            {
+                write($"{indent}{nameof(HorizOriginY)}: \"{HorizOriginY}\"");
+            }
+            if (HorizAdvX != null)
+            {
+                write($"{indent}{nameof(HorizAdvX)}: \"{HorizAdvX}\"");
+            }
+            if (VertOriginX != null)
+            {
+                write($"{indent}{nameof(VertOriginX)}: \"{VertOriginX}\"");
+            }
+            if (VertOriginY != null)
+            {
+                write($"{indent}{nameof(VertOriginY)}: \"{VertOriginY}\"");
+            }
+            if (VertAdvY != null)
+            {
+                write($"{indent}{nameof(VertAdvY)}: \"{VertAdvY}\"");
+            }
         }
     }
 }
