b87531f45ea4a013f1b63ebccf6393da1594529f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 20:20:16 +0100

    Update SvgScript.cs

--- src/SvgXml/Svg/Scripting/SvgScript.cs
+++ src/SvgXml/Svg/Scripting/SvgScript.cs
@@ -6,10 +6,32 @@ namespace Svg
     [Element("script")]
     public class SvgScript : SvgElement, ISvgResourcesAttributes
     {
+        [Attribute("type")]
+        public string? Type
+        {
+            get => GetAttribute("type");
+            set => SetAttribute("type", value);
+        }
+
+        [Attribute("href")]
+        public string? Href
+        {
+            get => GetAttribute("href");
+            set => SetAttribute("href", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (Type != null)
+            {
+                write($"{indent}{nameof(Type)}='{Type}'");
+            }
+            if (Href != null)
+            {
+                write($"{indent}{nameof(Href)}: \"{Href}\"");
+            }
         }
     }
 }
