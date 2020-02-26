7c24c5d1377bf7943675f43c17158b3bef241e10
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 23:02:05 +0100

    Update SvgCursor.cs

--- src/SvgXml/Svg/Interactivity/SvgCursor.cs
+++ src/SvgXml/Svg/Interactivity/SvgCursor.cs
@@ -6,10 +6,43 @@ namespace Svg
     [Element("cursor")]
     public class SvgCursor : SvgElement, ISvgTestsAttributes, ISvgResourcesAttributes
     {
+        [Attribute("x", SvgAttributes.SvgNamespace)]
+        public string? X
+        {
+            get => GetAttribute("x");
+            set => SetAttribute("x", value);
+        }
+
+        [Attribute("y", SvgAttributes.SvgNamespace)]
+        public string? Y
+        {
+            get => GetAttribute("y");
+            set => SetAttribute("y", value);
+        }
+
+        [Attribute("href", SvgAttributes.XLinkNamespace)]
+        public string? Href
+        {
+            get => GetAttribute("href");
+            set => SetAttribute("href", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (X != null)
+            {
+                write($"{indent}{nameof(X)}: \"{X}\"");
+            }
+            if (Y != null)
+            {
+                write($"{indent}{nameof(Y)}: \"{Y}\"");
+            }
+            if (Href != null)
+            {
+                write($"{indent}{nameof(Href)}: \"{Href}\"");
+            }
         }
     }
 }
