39da543e65debd50afd62933b3258945ef00cb2c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 22:53:33 +0100

    Update SvgAnchor.cs

--- src/SvgXml/Svg/Linking/SvgAnchor.cs
+++ src/SvgXml/Svg/Linking/SvgAnchor.cs
@@ -6,6 +6,27 @@ namespace Svg
     [Element("a")]
     public class SvgAnchor : SvgElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgResourcesAttributes, ISvgTransformableAttributes
     {
+        [Attribute("href", SvgAttributes.XLinkNamespace)]
+        public string? Href
+        {
+            get => GetAttribute("href");
+            set => SetAttribute("href", value);
+        }
+
+        [Attribute("show", SvgAttributes.XLinkNamespace)]
+        public string? Show
+        {
+            get => GetAttribute("show");
+            set => SetAttribute("show", value);
+        }
+
+        [Attribute("actuate", SvgAttributes.XLinkNamespace)]
+        public string? Actuate
+        {
+            get => GetAttribute("actuate");
+            set => SetAttribute("actuate", value);
+        }
+
         [Attribute("target", SvgAttributes.SvgNamespace)]
         public string? Target
         {
