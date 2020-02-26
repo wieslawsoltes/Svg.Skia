dc6cb06e115fe1adaa09796f10be9219d24a204f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 20:44:17 +0100

    Update SvgView.cs

--- src/SvgXml/Svg/Linking/SvgView.cs
+++ src/SvgXml/Svg/Linking/SvgView.cs
@@ -6,10 +6,54 @@ namespace Svg
     [Element("view")]
     public class SvgView : SvgElement, ISvgResourcesAttributes
     {
+        [Attribute("viewBox")]
+        public string? ViewBox
+        {
+            get => GetAttribute("viewBox");
+            set => SetAttribute("viewBox", value);
+        }
+
+        [Attribute("preserveAspectRatio")]
+        public string? AspectRatio
+        {
+            get => GetAttribute("preserveAspectRatio");
+            set => SetAttribute("preserveAspectRatio", value);
+        }
+
+        [Attribute("zoomAndPan")]
+        public string? ZoomAndPan
+        {
+            get => GetAttribute("zoomAndPan");
+            set => SetAttribute("zoomAndPan", value);
+        }
+
+        [Attribute("viewTarget")]
+        public string? ViewTarget
+        {
+            get => GetAttribute("viewTarget");
+            set => SetAttribute("viewTarget", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (ViewBox != null)
+            {
+                write($"{indent}{nameof(ViewBox)}: \"{ViewBox}\"");
+            }
+            if (AspectRatio != null)
+            {
+                write($"{indent}{nameof(AspectRatio)}: \"{AspectRatio}\"");
+            }
+            if (ZoomAndPan != null)
+            {
+                write($"{indent}{nameof(ZoomAndPan)}: \"{ZoomAndPan}\"");
+            }
+            if (ViewTarget != null)
+            {
+                write($"{indent}{nameof(ViewTarget)}: \"{ViewTarget}\"");
+            }
         }
     }
 }
