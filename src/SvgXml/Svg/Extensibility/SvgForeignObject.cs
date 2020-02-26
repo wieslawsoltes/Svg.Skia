dcc75639c42d9130fff3fc50e1c2907d037dc42a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 20:12:32 +0100

    Update SvgForeignObject.cs

--- src/SvgXml/Svg/Extensibility/SvgForeignObject.cs
+++ src/SvgXml/Svg/Extensibility/SvgForeignObject.cs
@@ -6,10 +6,54 @@ namespace Svg
     [Element("foreignObject")]
     public class SvgForeignObject : SvgVisualElement, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgResourcesAttributes, ISvgTransformableAttributes
     {
+        [Attribute("x")]
+        public string? X
+        {
+            get => GetAttribute("x");
+            set => SetAttribute("x", value);
+        }
+
+        [Attribute("y")]
+        public string? Y
+        {
+            get => GetAttribute("y");
+            set => SetAttribute("y", value);
+        }
+
+        [Attribute("width")]
+        public string? Width
+        {
+            get => GetAttribute("width");
+            set => SetAttribute("width", value);
+        }
+
+        [Attribute("height")]
+        public string? Height
+        {
+            get => GetAttribute("height");
+            set => SetAttribute("height", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (X != null)
+            {
+                write($"{indent}{nameof(X)}: \"{X}\"");
+            }
+            if (Y != null)
+            {
+                write($"{indent}{nameof(Y)}: \"{Y}\"");
+            }
+            if (Width != null)
+            {
+                write($"{indent}{nameof(Width)}: \"{Width}\"");
+            }
+            if (Height != null)
+            {
+                write($"{indent}{nameof(Height)}: \"{Height}\"");
+            }
         }
     }
 }
