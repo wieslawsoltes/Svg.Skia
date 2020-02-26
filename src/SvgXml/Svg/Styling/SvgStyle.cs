f54fd52301034125ee5cb4161cc0b67dca8d8f2f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-26 20:16:18 +0100

    Update SvgStyle.cs

--- src/SvgXml/Svg/Styling/SvgStyle.cs
+++ src/SvgXml/Svg/Styling/SvgStyle.cs
@@ -6,10 +6,43 @@ namespace Svg
     [Element("style")]
     public class SvgStyle : SvgElement
     {
+        [Attribute("type")]
+        public string? Type
+        {
+            get => GetAttribute("type");
+            set => SetAttribute("type", value);
+        }
+
+        [Attribute("media")]
+        public string? Media
+        {
+            get => GetAttribute("media");
+            set => SetAttribute("media", value);
+        }
+
+        [Attribute("title")]
+        public string? Title
+        {
+            get => GetAttribute("title");
+            set => SetAttribute("title", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (Type != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Type)}='{Type}'");
+            }
+            if (Media != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Media)}='{Media}'");
+            }
+            if (Title != null)
+            {
+                Console.WriteLine($"{indent}{nameof(Title)}='{Title}'");
+            }
         }
     }
 }
