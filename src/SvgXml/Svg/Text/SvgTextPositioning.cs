4e3d23d6103f7089e5b4e9ae4c34e4b67d75a191
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 01:41:53 +0100

    Create SvgTextPositioning.cs

---
+++ src/SvgXml/Svg/Text/SvgTextPositioning.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xml;
+
+namespace Svg
+{
+    public abstract class SvgTextPositioning : SvgTextContent
+    {
+        [Attribute("x", SvgAttributes.SvgNamespace)]
+        public string? X
+        {
+            get => GetAttribute("x");
+            set => SetAttribute("x", value);
+        }
+
+        [Attribute("y", SvgAttributes.SvgNamespace)]
+        public string? Y
+        {
+            get => GetAttribute("y");
+            set => SetAttribute("y", value);
+        }
+
+        [Attribute("dx", SvgAttributes.SvgNamespace)]
+        public string? Dx
+        {
+            get => GetAttribute("dx");
+            set => SetAttribute("dx", value);
+        }
+
+        [Attribute("dy", SvgAttributes.SvgNamespace)]
+        public string? Dy
+        {
+            get => GetAttribute("dy");
+            set => SetAttribute("dy", value);
+        }
+
+        [Attribute("rotate", SvgAttributes.SvgNamespace)]
+        public string? Rotate
+        {
+            get => GetAttribute("rotate");
+            set => SetAttribute("rotate", value);
+        }
+
+        public override void Print(Action<string> write, string indent)
+        {
+            base.Print(write, indent);
+
+            if (X != null)
+            {
+                write($"{indent}{nameof(X)}: \"{X}\"");
+            }
+            if (Y != null)
+            {
+                write($"{indent}{nameof(Y)}: \"{Y}\"");
+            }
+            if (Dx != null)
+            {
+                write($"{indent}{nameof(Dx)}: \"{Dx}\"");
+            }
+            if (Dy != null)
+            {
+                write($"{indent}{nameof(Dy)}: \"{Dy}\"");
+            }
+            if (Rotate != null)
+            {
+                write($"{indent}{nameof(Rotate)}: \"{Rotate}\"");
+            }
+        }
+    }
+}
