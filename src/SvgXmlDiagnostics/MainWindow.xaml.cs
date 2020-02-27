6c858c2247037a34a4bf605ec84c6e5a86b9948a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 18:30:11 +0100

    Added SvgXmlDiagnostics

---
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace SvgXmlDiagnostics
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
