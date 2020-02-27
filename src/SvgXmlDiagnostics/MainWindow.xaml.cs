698b5726be4e671585de5dbfb88b4107dad714bb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 19:00:56 +0100

    Update MainWindow.xaml.cs

--- src/SvgXmlDiagnostics/MainWindow.xaml.cs
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Svg;
 
 namespace SvgXmlDiagnostics
 {
@@ -11,12 +12,23 @@ namespace SvgXmlDiagnostics
 
         private void FileOpen_Click(object sender, RoutedEventArgs e)
         {
-
+            var dlg = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "Supported Files (*.svg;*.svgz)|*.svg;*svgz|Svg Files (*.svg)|*.svg|Svgz Files (*.svgz)|*.svgz|All Files (*.*)|*.*"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                var svgDocument = SvgDocument.Open(dlg.FileName);
+                if (svgDocument != null)
+                {
+                    DocumentTree.Items.Add(svgDocument);
+                }
+            }
         }
 
         private void FileExit_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
     }
 }
