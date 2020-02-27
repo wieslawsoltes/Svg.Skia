11cb5626b99564e8acd14cfe5937dfa61ccbf3ef
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-28 00:45:12 +0100

    Update MainWindow.xaml.cs

--- src/SvgXmlDiagnostics/MainWindow.xaml.cs
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
 using Svg;
 
 namespace SvgXmlDiagnostics
@@ -36,6 +38,24 @@ namespace SvgXmlDiagnostics
         private void FileExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+    }
+
+    public class TypeVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var parameterType = parameter as Type;
+            var valueType = value?.GetType();
+            if (parameterType?.IsAssignableFrom(valueType) == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
