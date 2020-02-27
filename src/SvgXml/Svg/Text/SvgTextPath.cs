802baed09059af8597a9794655ead327b1d5a06b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 01:41:48 +0100

    Update SvgTextPath.cs

--- src/SvgXml/Svg/Text/SvgTextPath.cs
+++ src/SvgXml/Svg/Text/SvgTextPath.cs
@@ -4,12 +4,56 @@ using Xml;
 namespace Svg
 {
     [Element("textPath")]
-    public class SvgTextPath : SvgTextBase, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgResourcesAttributes
+    public class SvgTextPath : SvgTextContent, ISvgPresentationAttributes, ISvgTestsAttributes, ISvgStylableAttributes, ISvgResourcesAttributes
     {
+        [Attribute("href", SvgAttributes.XLinkNamespace)]
+        public string? Href
+        {
+            get => GetAttribute("href");
+            set => SetAttribute("href", value);
+        }
+
+        [Attribute("startOffset", SvgAttributes.SvgNamespace)]
+        public string? StartOffset
+        {
+            get => GetAttribute("startOffset");
+            set => SetAttribute("startOffset", value);
+        }
+
+        [Attribute("method", SvgAttributes.SvgNamespace)]
+        public string? Method
+        {
+            get => GetAttribute("method");
+            set => SetAttribute("method", value);
+        }
+
+        [Attribute("spacing", SvgAttributes.SvgNamespace)]
+        public string? Spacing
+        {
+            get => GetAttribute("spacing");
+            set => SetAttribute("spacing", value);
+        }
+
         public override void Print(Action<string> write, string indent)
         {
             base.Print(write, indent);
-            // TODO:
+
+            if (StartOffset != null)
+            {
+                write($"{indent}{nameof(StartOffset)}: \"{StartOffset}\"");
+            }
+            if (Method != null)
+            {
+                write($"{indent}{nameof(Method)}: \"{Method}\"");
+            }
+            if (Spacing != null)
+            {
+                write($"{indent}{nameof(Spacing)}: \"{Spacing}\"");
+            }
+            if (Href != null)
+            {
+                write($"{indent}{nameof(Href)}: \"{Href}\"");
+            }
         }
     }
 }
