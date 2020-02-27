e47e5752c4376d22bbd97cd19ee0e7fd9de904df
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 01:41:23 +0100

    Update SvgVerticalKern.cs

--- src/SvgXml/Svg/Text/SvgVerticalKern.cs
+++ src/SvgXml/Svg/Text/SvgVerticalKern.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using Xml;
+﻿using Xml;
 
 namespace Svg
 {
     [Element("vkern")]
     public class SvgVerticalKern : SvgKern
     {
-        public override void Print(Action<string> write, string indent)
-        {
-            base.Print(write, indent);
-            // TODO:
-        }
     }
 }
