fff4ea2a1cb9a75330b4f48b34cf0ec319aa7395
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-27 01:01:38 +0100

    Update SvgFontFaceSrc.cs

--- src/SvgXml/Svg/Text/SvgFontFaceSrc.cs
+++ src/SvgXml/Svg/Text/SvgFontFaceSrc.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using Xml;
+﻿using Xml;
 
 namespace Svg
 {
     [Element("font-face-src")]
     public class SvgFontFaceSrc : SvgElement
     {
-        public override void Print(Action<string> write, string indent)
-        {
-            base.Print(write, indent);
-            // TODO:
-        }
     }
 }
