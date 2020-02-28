7bec18788ebf4f95523fcbcec33c7c974bde0f6e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-28 22:03:23 +0100

    Update MainWindow.xaml.cs

--- src/SvgXmlDiagnostics/MainWindow.xaml.cs
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using Svg;
 
@@ -14,6 +18,7 @@ namespace SvgXmlDiagnostics
 
     public partial class MainWindow : Window
     {
+        private volatile bool _isProcessing = false;
         public ObservableCollection<Item> Items { get; set; }
 
         public MainWindow()
@@ -25,6 +30,10 @@ namespace SvgXmlDiagnostics
 
         private void FileOpen_Click(object sender, RoutedEventArgs e)
         {
+            if (_isProcessing)
+            {
+                return;
+            }
             var dlg = new Microsoft.Win32.OpenFileDialog()
             {
                 Filter = "Supported Files (*.svg;*.svgz)|*.svg;*svgz" +
@@ -35,20 +44,37 @@ namespace SvgXmlDiagnostics
             };
             if (dlg.ShowDialog() == true)
             {
-                foreach (var fileName in dlg.FileNames)
+                _isProcessing = true;
+                var paths = dlg.FileNames.ToList();
+                Task.Factory.StartNew(() =>
                 {
-                    var svgDocument = SvgDocument.Open(fileName);
-                    if (svgDocument != null)
+                    try
                     {
-                        var item = new Item()
+                        foreach (var path in paths)
                         {
-                            Name = Path.GetFileNameWithoutExtension(fileName),
-                            Path = fileName,
-                            Document = svgDocument
-                        };
-                        Items.Add(item);
+                            var svgDocument = SvgDocument.Open(path);
+                            if (svgDocument != null)
+                            {
+                                var item = new Item()
+                                {
+                                    Name = Path.GetFileNameWithoutExtension(path),
+                                    Path = path,
+                                    Document = svgDocument
+                                };
+                                Dispatcher.Invoke(() => Items.Add(item));
+                            }
+                        }
                     }
-                }
+                    catch(Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        Debug.WriteLine(ex.StackTrace);
+                    }
+                    finally
+                    {
+                        _isProcessing = false;
+                    }
+                });
             }
         }
 
