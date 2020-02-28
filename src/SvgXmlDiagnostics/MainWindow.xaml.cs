1145a4c4220bec942966cd29acfd007dff04507e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-28 21:57:40 +0100

    Updated MainWindow

--- src/SvgXmlDiagnostics/MainWindow.xaml.cs
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -1,13 +1,26 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
 using Svg;
 
 namespace SvgXmlDiagnostics
 {
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public SvgDocument Document { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Item> Items { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Items = new ObservableCollection<Item>();
+            DataContext = this;
         }
 
         private void FileOpen_Click(object sender, RoutedEventArgs e)
@@ -27,7 +40,13 @@ namespace SvgXmlDiagnostics
                     var svgDocument = SvgDocument.Open(fileName);
                     if (svgDocument != null)
                     {
-                        DocumentTree.Items.Add(svgDocument);
+                        var item = new Item()
+                        {
+                            Name = Path.GetFileNameWithoutExtension(fileName),
+                            Path = fileName,
+                            Document = svgDocument
+                        };
+                        Items.Add(item);
                     }
                 }
             }
