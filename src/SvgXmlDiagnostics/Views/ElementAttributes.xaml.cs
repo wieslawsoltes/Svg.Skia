686aa75183a337c2a7c48e4f8a29a8f0d20fb069
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-28 21:57:24 +0100

    Added ElementAttributes

---
+++ src/SvgXmlDiagnostics/Views/ElementAttributes.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SvgXmlDiagnostics.Views
+{
+    public partial class ElementAttributes : UserControl
+    {
+        public ElementAttributes()
+        {
+            InitializeComponent();
+        }
+    }
+}
