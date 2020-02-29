03cb679a73b2d718cbcbdc17a45271ba194b56f1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-29 21:02:33 +0100

    Update SvgMarker.cs

--- src/SvgXml/Svg/Painting/SvgMarker.cs
+++ src/SvgXml/Svg/Painting/SvgMarker.cs
@@ -13,56 +13,56 @@ namespace Svg
         [Attribute("viewBox", SvgNamespace)]
         public string? ViewBox
         {
-            get => this.GetAttribute("viewBox");
+            get => this.GetAttribute("viewBox", false, null);
             set => this.SetAttribute("viewBox", value);
         }
 
         [Attribute("preserveAspectRatio", SvgNamespace)]
         public string? AspectRatio
         {
-            get => this.GetAttribute("preserveAspectRatio");
+            get => this.GetAttribute("preserveAspectRatio", false, "xMidYMid meet");
             set => this.SetAttribute("preserveAspectRatio", value);
         }
 
         [Attribute("refX", SvgNamespace)]
         public string? RefX
         {
-            get => this.GetAttribute("refX");
+            get => this.GetAttribute("refX", false, "0");
             set => this.SetAttribute("refX", value);
         }
 
         [Attribute("refY", SvgNamespace)]
         public string? RefY
         {
-            get => this.GetAttribute("refY");
+            get => this.GetAttribute("refY", false, "0");
             set => this.SetAttribute("refY", value);
         }
 
         [Attribute("markerUnits", SvgNamespace)]
         public string? MarkerUnits
         {
-            get => this.GetAttribute("markerUnits");
+            get => this.GetAttribute("markerUnits", false, "strokeWidth");
             set => this.SetAttribute("markerUnits", value);
         }
 
         [Attribute("markerWidth", SvgNamespace)]
         public string? MarkerWidth
         {
-            get => this.GetAttribute("markerWidth");
+            get => this.GetAttribute("markerWidth", false, "3");
             set => this.SetAttribute("markerWidth", value);
         }
 
         [Attribute("markerHeight", SvgNamespace)]
         public string? MarkerHeight
         {
-            get => this.GetAttribute("markerHeight");
+            get => this.GetAttribute("markerHeight", false, "3");
             set => this.SetAttribute("markerHeight", value);
         }
 
         [Attribute("orient", SvgNamespace)]
         public string? Orient
         {
-            get => this.GetAttribute("orient");
+            get => this.GetAttribute("orient", false, "0");
             set => this.SetAttribute("orient", value);
         }
 
