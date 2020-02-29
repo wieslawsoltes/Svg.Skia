60df13561528fa2e24032ccb46e43616d83b0787
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-29 21:02:37 +0100

    Update SvgPatternServer.cs

--- src/SvgXml/Svg/Painting/SvgPatternServer.cs
+++ src/SvgXml/Svg/Painting/SvgPatternServer.cs
@@ -14,70 +14,70 @@ namespace Svg
         [Attribute("viewBox", SvgNamespace)]
         public string? ViewBox
         {
-            get => this.GetAttribute("viewBox");
+            get => this.GetAttribute("viewBox", false, null);
             set => this.SetAttribute("viewBox", value);
         }
 
         [Attribute("preserveAspectRatio", SvgNamespace)]
         public string? AspectRatio
         {
-            get => this.GetAttribute("preserveAspectRatio");
+            get => this.GetAttribute("preserveAspectRatio", false, "xMidYMid meet");
             set => this.SetAttribute("preserveAspectRatio", value);
         }
 
         [Attribute("x", SvgNamespace)]
         public string? X
         {
-            get => this.GetAttribute("x");
+            get => this.GetAttribute("x", false, "0");
             set => this.SetAttribute("x", value);
         }
 
         [Attribute("y", SvgNamespace)]
         public string? Y
         {
-            get => this.GetAttribute("y");
+            get => this.GetAttribute("y", false, "0");
             set => this.SetAttribute("y", value);
         }
 
         [Attribute("width", SvgNamespace)]
         public string? Width
         {
-            get => this.GetAttribute("width");
+            get => this.GetAttribute("width", false, "0");
             set => this.SetAttribute("width", value);
         }
 
         [Attribute("height", SvgNamespace)]
         public string? Height
         {
-            get => this.GetAttribute("height");
+            get => this.GetAttribute("height", false, "0");
             set => this.SetAttribute("height", value);
         }
 
         [Attribute("patternUnits", SvgNamespace)]
         public string? PatternUnits
         {
-            get => this.GetAttribute("patternUnits");
+            get => this.GetAttribute("patternUnits", false, "objectBoundingBox");
             set => this.SetAttribute("patternUnits", value);
         }
 
         [Attribute("patternContentUnits", SvgNamespace)]
         public string? PatternContentUnits
         {
-            get => this.GetAttribute("patternContentUnits");
+            get => this.GetAttribute("patternContentUnits", false, "userSpaceOnUse");
             set => this.SetAttribute("patternContentUnits", value);
         }
 
         [Attribute("patternTransform", SvgNamespace)]
         public string? PatternTransform
         {
-            get => this.GetAttribute("patternTransform");
+            get => this.GetAttribute("patternTransform", false, null);
             set => this.SetAttribute("patternTransform", value);
         }
 
         [Attribute("href", XLinkNamespace)]
         public string? Href
         {
-            get => this.GetAttribute("href");
+            get => this.GetAttribute("href", false, null);
             set => this.SetAttribute("href", value);
         }
 
