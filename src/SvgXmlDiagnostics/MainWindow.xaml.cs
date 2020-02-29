7f69de45c0056cde77fd744f1514fde97074be75
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-29 21:02:53 +0100

    Update MainWindow.xaml.cs

--- src/SvgXmlDiagnostics/MainWindow.xaml.cs
+++ src/SvgXmlDiagnostics/MainWindow.xaml.cs
@@ -18,6 +18,19 @@ namespace SvgXmlDiagnostics
         public SvgDocument Document { get; set; }
     }
 
+    public class TypeNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.GetType().Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public partial class MainWindow : Window
     {
         private volatile bool _isProcessing = false;
