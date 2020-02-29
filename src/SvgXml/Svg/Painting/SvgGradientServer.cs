ce611a4e8651f7cbd8b0bb7193bc60ed6f53f5a7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-02-29 21:02:19 +0100

    Update SvgGradientServer.cs

--- src/SvgXml/Svg/Painting/SvgGradientServer.cs
+++ src/SvgXml/Svg/Painting/SvgGradientServer.cs
@@ -1,6 +1,58 @@
-﻿namespace Svg
+﻿using System;
+using Xml;
+
+namespace Svg
 {
     public abstract class SvgGradientServer : SvgPaintServer
     {
+        [Attribute("gradientUnits", SvgNamespace)]
+        public string? GradientUnits
+        {
+            get => this.GetAttribute("gradientUnits", false, "objectBoundingBox");
+            set => this.SetAttribute("gradientUnits", value);
+        }
+
+        [Attribute("gradientTransform", SvgNamespace)]
+        public string? GradientTransform
+        {
+            get => this.GetAttribute("gradientTransform", false, null);
+            set => this.SetAttribute("gradientTransform", value);
+        }
+
+        [Attribute("spreadMethod", SvgNamespace)]
+        public string? SpreadMethod
+        {
+            get => this.GetAttribute("spreadMethod", false, "pad");
+            set => this.SetAttribute("spreadMethod", value);
+        }
+
+        [Attribute("href", XLinkNamespace)]
+        public string? Href
+        {
+            get => this.GetAttribute("href", false, null);
+            set => this.SetAttribute("href", value);
+        }
+
+        public override void Print(Action<string> write, string indent)
+        {
+            base.Print(write, indent);
+
+            if (GradientUnits != null)
+            {
+                write($"{indent}{nameof(GradientUnits)}: \"{GradientUnits}\"");
+            }
+            if (GradientTransform != null)
+            {
+                write($"{indent}{nameof(GradientTransform)}: \"{GradientTransform}\"");
+            }
+            if (SpreadMethod != null)
+            {
+                write($"{indent}{nameof(SpreadMethod)}: \"{SpreadMethod}\"");
+            }
+            if (Href != null)
+            {
+                write($"{indent}{nameof(Href)}: \"{Href}\"");
+            }
+        }
     }
 }
