518791824c0e879d5a906755893845e8982ff937
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-01 23:12:08 +0100

    Create ElementStyles.xaml.cs

---
+++ src/SvgXmlDiagnostics/Views/ElementStyles.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SvgXmlDiagnostics.Views
+{
+    public partial class ElementStyles : UserControl
+    {
+        public ElementStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
