a6eeffbdf738a387982b48615d1c2a60a74a4a45
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-01 20:51:55 +0100

    Update IElementFactory.cs

--- src/SvgXml/Xml/IElementFactory.cs
+++ src/SvgXml/Xml/IElementFactory.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Xml
 {
     public interface IElementFactory
     {
+        Dictionary<string, Type> Types { get; }
         ISet<string> Namespaces { get; }
         Element Create(string name, IElement? parent);
     }
