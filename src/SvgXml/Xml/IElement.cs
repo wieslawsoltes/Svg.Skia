7d73e63d432c4414de2711b1d24d0ea8173c7114
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-01 23:10:14 +0100

    Update IElement.cs

--- src/SvgXml/Xml/IElement.cs
+++ src/SvgXml/Xml/IElement.cs
@@ -8,8 +8,13 @@ namespace Xml
         string Content { get; set; }
         List<Element> Children { get; set; }
         Dictionary<string, string?> Attributes { get; set; }
+        Dictionary<string, SortedDictionary<int, string>> Styles { get; set; }
         IElement? Parent { get; set; }
+        void SetPropertyValue(string key, string? value);
         string? GetAttribute(string key);
         void SetAttribute(string key, string? value);
+        bool ContainsAttribute(string key);
+        bool TryGetAttribute(string key, out string? value);
+        void AddStyle(string name, string value, int specificity);
     }
 }
