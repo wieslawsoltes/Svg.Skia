82d8ba7b4237140d9d1d475444238f80da7b1b8b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-01 23:10:32 +0100

    Update SvgElement.cs

--- src/SvgXml/Svg/SvgElement.cs
+++ src/SvgXml/Svg/SvgElement.cs
@@ -3,34 +3,6 @@ using Xml;
 
 namespace Svg
 {
-    public static class SvgElementExtensions
-    {
-        public static string? GetAttribute(this IElement element, string key, bool inherited, string? defaultValue)
-        {
-            bool inherit = false;
-
-            if (element.Attributes.TryGetValue(key, out var value))
-            {
-                inherit = string.Equals(value?.ToString(), "inherit", StringComparison.OrdinalIgnoreCase);
-                if (!inherit)
-                {
-                    return value;
-                }
-            }
-
-            if (inherited || inherit)
-            {
-                var parentValue = element.Parent?.GetAttribute(key, inherited, default);
-                if (parentValue != null)
-                {
-                    return parentValue;
-                }
-            }
-
-            return defaultValue;
-        }
-    }
-
     public abstract class SvgElement : Element
     {
         public const string SvgNamespace = "http://www.w3.org/2000/svg";
