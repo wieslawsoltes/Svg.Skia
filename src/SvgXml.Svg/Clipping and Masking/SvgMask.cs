cf9d0d60e8ca9e050b16af713b1cdbc5805cd786
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-08 22:06:39 +0100

    Update SvgMask.cs

--- src/SvgXml.Svg/Clipping and Masking/SvgMask.cs
+++ src/SvgXml.Svg/Clipping and Masking/SvgMask.cs
@@ -14,42 +14,42 @@ namespace Svg
         [Attribute("x", SvgNamespace)]
         public string? X
         {
-            get => this.GetAttribute("x");
+            get => this.GetAttribute("x", false, "-10%");
             set => this.SetAttribute("x", value);
         }
 
         [Attribute("y", SvgNamespace)]
         public string? Y
         {
-            get => this.GetAttribute("y");
+            get => this.GetAttribute("y", false, "-10%");
             set => this.SetAttribute("y", value);
         }
 
         [Attribute("width", SvgNamespace)]
         public string? Width
         {
-            get => this.GetAttribute("width");
+            get => this.GetAttribute("width", false, "120%");
             set => this.SetAttribute("width", value);
         }
 
         [Attribute("height", SvgNamespace)]
         public string? Height
         {
-            get => this.GetAttribute("height");
+            get => this.GetAttribute("height", false, "120%");
             set => this.SetAttribute("height", value);
         }
 
         [Attribute("maskUnits", SvgNamespace)]
         public string? MaskUnits
         {
-            get => this.GetAttribute("maskUnits");
+            get => this.GetAttribute("maskUnits", false, "objectBoundingBox");
             set => this.SetAttribute("maskUnits", value);
         }
 
         [Attribute("maskContentUnits", SvgNamespace)]
         public string? MaskContentUnits
         {
-            get => this.GetAttribute("maskContentUnits");
+            get => this.GetAttribute("maskContentUnits", false, "userSpaceOnUse");
             set => this.SetAttribute("maskContentUnits", value);
         }
 
