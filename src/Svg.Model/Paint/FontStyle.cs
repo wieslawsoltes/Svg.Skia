eb357b8a063eeeb28042424d2663bb09c2e618c6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-12 20:10:57 +0200

    Delete FontStyle.cs

--- src/Svg.Model/Paint/FontStyle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Svg.Model
-{
-    public class FontStyle
-    {
-        public int Weight { get; }
-        public int Width { get; }
-        public FontStyleSlant Slant { get; }
-    }
-}
