1d10c49b679601bf89e1eba6b0d83b7e20da24ce
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-12 21:29:46 +0200

    Update PaintImageFilter.cs

--- src/Svg.Model/ImageFilters/PaintImageFilter.cs
+++ src/Svg.Model/ImageFilters/PaintImageFilter.cs
@@ -2,13 +2,7 @@
 {
     public class PaintImageFilter : ImageFilter
     {
-        public Paint Paint { get; set; }
-        public CropRect CropRect { get; set; }
-
-        public PaintImageFilter(Paint paint, CropRect cropRect)
-        {
-            Paint = paint;
-            CropRect = cropRect;
-        }
+        public Paint? Paint { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
