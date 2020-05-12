21402d161a5f868d9f69b4a26a94dd086adbdcbf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-12 20:11:04 +0200

    Create FontStyleWidth.cs

---
+++ src/Svg.Model/Paint/FontStyleWidth.cs
@@ -0,0 +1,15 @@
+﻿namespace Svg.Model
+{
+    public enum FontStyleWidth
+    {
+        UltraCondensed = 1,
+        ExtraCondensed = 2,
+        Condensed = 3,
+        SemiCondensed = 4,
+        Normal = 5,
+        SemiExpanded = 6,
+        Expanded = 7,
+        ExtraExpanded = 8,
+        UltraExpanded = 9
+    }
+}
