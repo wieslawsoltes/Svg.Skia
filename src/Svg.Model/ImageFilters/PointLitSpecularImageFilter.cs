e1b2db74a04655304885b4b37b9bbafe6affd6c8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 13:25:11 +0200

    Update PointLitSpecularImageFilter.cs

--- src/Svg.Model/ImageFilters/PointLitSpecularImageFilter.cs
+++ src/Svg.Model/ImageFilters/PointLitSpecularImageFilter.cs
@@ -2,6 +2,12 @@
 {
     public class PointLitSpecularImageFilter : ImageFilter
     {
-        // TODO:
+        public Point3 Location { get; set; }
+        public Color LightColor { get; set; }
+        public float SurfaceScale { get; set; }
+        public float KS { get; set; }
+        public float Shininess { get; set; }
+        public ImageFilter? Input { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
