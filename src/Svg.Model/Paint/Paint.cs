82bf096ef2418ee1df6846945c77fc8313e4df93
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 18:37:31 +0200

    Update Paint.cs

--- src/Svg.Model/Paint/Paint.cs
+++ src/Svg.Model/Paint/Paint.cs
@@ -2,21 +2,21 @@
 {
     public class Paint
     {
-        public PaintStyle Style { get; set; }
-        public bool IsAntialias { get; set; }
-        public float StrokeWidth { get; set; }
-        public StrokeCap StrokeCap { get; set; }
-        public StrokeJoin StrokeJoin { get; set; }
-        public float StrokeMiter { get; set; }
-        public Typeface? Typeface { get; set; }
-        public float TextSize { get; set; }
-        public TextAlign TextAlign { get; set; }
-        public Color? Color { get; set; }
-        public Shader? Shader { get; set; }
-        public ColorFilter? ColorFilter { get; set; }
-        public ImageFilter? ImageFilter { get; set; }
-        public PathEffect? PathEffect { get; set; }
-        public BlendMode BlendMode { get; set; }
-        public FilterQuality FilterQuality { get; set; }
+        public PaintStyle Style;
+        public bool IsAntialias;
+        public float StrokeWidth;
+        public StrokeCap StrokeCap;
+        public StrokeJoin StrokeJoin;
+        public float StrokeMiter;
+        public Typeface? Typeface;
+        public float TextSize;
+        public TextAlign TextAlign;
+        public Color? Color;
+        public Shader? Shader;
+        public ColorFilter? ColorFilter;
+        public ImageFilter? ImageFilter;
+        public PathEffect? PathEffect;
+        public BlendMode BlendMode;
+        public FilterQuality FilterQuality;
     }
 }
