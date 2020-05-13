3586a84378e8b6ab2337ef4454d2b92a8f2bd25f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 11:50:47 +0200

    Update ArithmeticImageFilter.cs

--- src/Svg.Model/ImageFilters/ArithmeticImageFilter.cs
+++ src/Svg.Model/ImageFilters/ArithmeticImageFilter.cs
@@ -2,6 +2,13 @@
 {
     public class ArithmeticImageFilter : ImageFilter
     {
-        // TODO:
+        public float K1 { get; set; }
+        public float K2 { get; set; }
+        public float K3 { get; set; }
+        public float K4 { get; set; }
+        public bool EforcePMColor { get; set; }
+        public ImageFilter? Background { get; set; }
+        public ImageFilter? Foreground { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
