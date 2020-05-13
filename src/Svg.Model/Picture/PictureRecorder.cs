62226500910c97a5964cfcea9a9a4c5aae513fec
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 21:13:59 +0200

    Create PictureRecorder.cs

---
+++ src/Svg.Model/Picture/PictureRecorder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Svg.Model
+{
+    public class PictureRecorder : IDisposable
+    {
+        private Canvas _recordingCanvas;
+        private Rect _cullRect;
+
+        public Canvas RecordingCanvas => _recordingCanvas;
+
+        public PictureRecorder()
+        {
+            _recordingCanvas = new Canvas()
+            {
+                Commands = new List<PictureCommand>()
+            };
+        }
+
+        public Canvas BeginRecording(Rect cullRect)
+        {
+            _cullRect = cullRect;
+            _recordingCanvas.Commands?.Clear();
+            return _recordingCanvas;
+        }
+
+        public Picture EndRecording()
+        {
+            return new Picture()
+            {
+                CullRect = _cullRect,
+                Commands = _recordingCanvas.Commands
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
