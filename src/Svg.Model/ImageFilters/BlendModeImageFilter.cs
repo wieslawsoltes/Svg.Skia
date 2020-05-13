3aff016df0e1cddd985d336ea623e70b7a7bb14f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 11:50:51 +0200

    Update BlendModeImageFilter.cs

--- src/Svg.Model/ImageFilters/BlendModeImageFilter.cs
+++ src/Svg.Model/ImageFilters/BlendModeImageFilter.cs
@@ -2,6 +2,9 @@
 {
     public class BlendModeImageFilter : ImageFilter
     {
-        // TODO:
+        public BlendMode Mode { get; set; }
+        public ImageFilter? Background { get; set; }
+        public ImageFilter? Foreground { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
