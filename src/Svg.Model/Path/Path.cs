42b1e97986614af6472c1337bd1abdb91d360df6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 21:44:05 +0200

    Update Path.cs

--- src/Svg.Model/Path/Path.cs
+++ src/Svg.Model/Path/Path.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Svg.Model
 {
-    public class Path
+    public class Path : IDisposable
     {
         public PathFillType FillType;
         public IList<PathCommand>? Commands;
+
+        public void Dispose()
+        {
+        }
     }
 }
