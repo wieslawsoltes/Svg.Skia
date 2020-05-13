8fe2a7af1bce03ec97be9cc2c4dd5c0b58347439
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 19:51:37 +0200

    Create Drawable.cs

---
+++ src/Svg.Model/Picture/Drawable.cs
@@ -0,0 +1,18 @@
+﻿namespace Svg.Model
+{
+    public abstract class Drawable
+    {
+        protected virtual void OnDraw(Canvas canvas)
+        {
+        }
+
+        protected virtual Rect OnGetBounds()
+        {
+            return Rect.Empty;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
