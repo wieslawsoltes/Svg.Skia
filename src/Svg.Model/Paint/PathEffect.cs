f6a7263560a09a40989b0f633366ec36b1477f9f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 19:08:23 +0200

    Update PathEffect.cs

--- src/Svg.Model/Paint/PathEffect.cs
+++ src/Svg.Model/Paint/PathEffect.cs
@@ -2,5 +2,13 @@
 {
     public abstract class PathEffect
     {
+        public static PathEffect CreateDash(float[] intervals, float phase)
+        {
+            return new DashPathEffect()
+            {
+                Intervals = intervals,
+                Phase = phase
+            };
+        }
     }
 }
