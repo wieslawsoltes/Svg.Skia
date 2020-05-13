10a2d6b6ccfde1b0c89ed79d63051a27940bf360
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 18:52:12 +0200

    Create Size.cs

---
+++ src/Svg.Model/Primitives/Size.cs
@@ -0,0 +1,16 @@
+﻿namespace Svg.Model
+{
+    public struct Size
+    {
+        public float Width;
+        public float Height;
+
+        public static readonly Size Empty;
+
+        public Size(float width, float height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
