abec21a236e2c088ce1ff8825586313e25c4198d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 11:50:59 +0200

    Update MatrixConvolutionImageFilter.cs

--- src/Svg.Model/ImageFilters/MatrixConvolutionImageFilter.cs
+++ src/Svg.Model/ImageFilters/MatrixConvolutionImageFilter.cs
@@ -2,6 +2,14 @@
 {
     public class MatrixConvolutionImageFilter : ImageFilter
     {
-        // TODO:
+        public SizeI KernelSize { get; set; }
+        public float[]? Kernel { get; set; }
+        public float Gain { get; set; }
+        public float Bias { get; set; }
+        public PointI KernelOffset { get; set; }
+        public MatrixConvolutionTileMode TileMode { get; set; }
+        public bool ConvolveAlpha { get; set; }
+        public ImageFilter? Input { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
