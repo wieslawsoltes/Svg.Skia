d7efd2561f5f5c635acb1c294e2b67143c20267b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 13:25:18 +0200

    Update SpotLitSpecularImageFilter.cs

--- src/Svg.Model/ImageFilters/SpotLitSpecularImageFilter.cs
+++ src/Svg.Model/ImageFilters/SpotLitSpecularImageFilter.cs
@@ -2,6 +2,15 @@
 {
     public class SpotLitSpecularImageFilter : ImageFilter
     {
-        // TODO:
+        public Point3 Location { get; set; }
+        public Point3 Target { get; set; }
+        public float SpecularExponent { get; set; }
+        public float CutoffAngle { get; set; }
+        public Color LightColor { get; set; }
+        public float SurfaceScale { get; set; }
+        public float KS { get; set; }
+        public float Shininess { get; set; }
+        public ImageFilter? Input { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
