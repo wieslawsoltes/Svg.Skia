f249f92eec700fc324f19ef6cc151f90211949e6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 13:24:50 +0200

    Update ErodeImageFilter.cs

--- src/Svg.Model/ImageFilters/ErodeImageFilter.cs
+++ src/Svg.Model/ImageFilters/ErodeImageFilter.cs
@@ -2,6 +2,9 @@
 {
     public class ErodeImageFilter : ImageFilter
     {
-        // TODO:
+        public int RadiusX { get; set; }
+        public int RadiusY { get; set; }
+        public ImageFilter? Input { get; set; }
+        public CropRect? CropRect { get; set; }
     }
 }
