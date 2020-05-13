b3d4a0f8634720f5025ed4c12517d1d483b6d414
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 21:13:27 +0200

    Update PathEffect.cs

--- src/Svg.Model/Paint/PathEffect.cs
+++ src/Svg.Model/Paint/PathEffect.cs
@@ -1,7 +1,13 @@
-﻿namespace Svg.Model
+﻿using System;
+
+namespace Svg.Model
 {
-    public abstract class PathEffect
+    public abstract class PathEffect : IDisposable
     {
+        public void Dispose()
+        {
+        }
+
         public static PathEffect CreateDash(float[] intervals, float phase)
         {
             return new DashPathEffect()
