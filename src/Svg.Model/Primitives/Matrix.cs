b256faebd50639b83e2eb94451459eb0d85206c0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-13 18:36:36 +0200

    Update Matrix.cs

--- src/Svg.Model/Primitives/Matrix.cs
+++ src/Svg.Model/Primitives/Matrix.cs
@@ -2,14 +2,31 @@
 {
     public struct Matrix
     {
-        public float ScaleX { get; set; }
-        public float SkewX { get; set; }
-        public float TransX { get; set; }
-        public float ScaleY { get; set; }
-        public float SkewY { get; set; }
-        public float TransY { get; set; }
-        public float Persp0 { get; set; }
-        public float Persp1 { get; set; }
-        public float Persp2 { get; set; }
+        public float ScaleX;
+        public float SkewX;
+        public float TransX;
+        public float ScaleY;
+        public float SkewY;
+        public float TransY;
+        public float Persp0;
+        public float Persp1;
+        public float Persp2;
+
+        public readonly static Matrix Empty;
+
+        public readonly static Matrix Identity = new Matrix { ScaleX = 1, ScaleY = 1, Persp2 = 1 };
+
+        public Matrix(float scaleX, float skewX, float transX, float skewY, float scaleY, float transY, float persp0, float persp1, float persp2)
+        {
+            ScaleX = scaleX;
+            SkewX = skewX;
+            TransX = transX;
+            SkewY = skewY;
+            ScaleY = scaleY;
+            TransY = transY;
+            Persp0 = persp0;
+            Persp1 = persp1;
+            Persp2 = persp2;
+        }
     }
 }
