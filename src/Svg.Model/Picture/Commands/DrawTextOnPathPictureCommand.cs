017ed95b95148b81b831491652f90c83b66a967a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-14 23:52:45 +0200

    Create DrawTextOnPathPictureCommand.cs

---
+++ src/Svg.Model/Picture/Commands/DrawTextOnPathPictureCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace Svg.Model
+{
+    public class DrawTextOnPathPictureCommand : PictureCommand
+    {
+        public string Text;
+        public Path Path;
+        public float HOffset;
+        public float VOffset;
+        public Paint Paint;
+
+        public DrawTextOnPathPictureCommand(string text, Path path, float hOffset, float vOffset, Paint paint)
+        {
+            Text = text;
+            Path = path;
+            HOffset = hOffset;
+            VOffset = vOffset;
+            Paint = paint;
+        }
+    }
+}
