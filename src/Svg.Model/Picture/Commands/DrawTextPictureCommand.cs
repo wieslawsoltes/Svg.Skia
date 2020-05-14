fbed5331bec67a4b13442cc760e454e078048174
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-14 23:43:46 +0200

    Create DrawTextPictureCommand.cs

---
+++ src/Svg.Model/Picture/Commands/DrawTextPictureCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Svg.Model
+{
+    public class DrawTextPictureCommand : PictureCommand
+    {
+        public string Text;
+        public float X;
+        public float Y;
+        public Paint Paint;
+
+        public DrawTextPictureCommand(string text, float x, float y, Paint paint)
+        {
+            Text = text;
+            X = x;
+            Y = y;
+            Paint = paint;
+        }
+    }
+}
