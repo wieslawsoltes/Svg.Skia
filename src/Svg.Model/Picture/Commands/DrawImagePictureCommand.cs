9b937b13a1dbe1edba60d6748874dba00b0ae7d0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-15 00:29:06 +0200

    Create DrawImagePictureCommand.cs

---
+++ src/Svg.Model/Picture/Commands/DrawImagePictureCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Svg.Model
+{
+    public class DrawImagePictureCommand : PictureCommand
+    {
+        public Image Image;
+        public Rect Source;
+        public Rect Dest;
+        public Paint? Paint;
+
+        public DrawImagePictureCommand(Image image, Rect source, Rect dest, Paint? paint = null)
+        {
+            Image = image;
+            Source = source;
+            Dest = dest;
+            Paint = paint;
+        }
+    }
+}
