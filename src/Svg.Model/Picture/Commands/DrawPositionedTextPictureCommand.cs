6f8ca415b2e2ccf73e8ce13ec45b65677d60b60e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-14 23:48:38 +0200

    Create DrawPositionedTextPictureCommand.cs

---
+++ src/Svg.Model/Picture/Commands/DrawPositionedTextPictureCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Svg.Model
+{
+    public class DrawPositionedTextPictureCommand : PictureCommand
+    {
+        public string Text;
+        public Point[] Points;
+        public Paint Paint;
+
+        public DrawPositionedTextPictureCommand(string text, Point[] points, Paint paint)
+        {
+            Text = text;
+            Points = points;
+            Paint = paint;
+        }
+    }
+}
