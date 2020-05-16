95735b62dcb05ebb3f23dc4cc9cf67a94c119ab7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:30:47 +0200

    Update ArcToPathCommand.cs

--- src/Svg.Model/Path/Commands/ArcToPathCommand.cs
+++ src/Svg.Model/Path/Commands/ArcToPathCommand.cs
@@ -2,12 +2,23 @@
 {
     public class ArcToPathCommand : PathCommand
     {
-        public float RX;
-        public float RY;
+        public float Rx;
+        public float Ry;
         public float XAxisRotate;
         public PathArcSize LargeArc;
         public PathDirection Sweep;
         public float X;
         public float Y;
+
+        public ArcToPathCommand(float rx, float ry, float xAxisRotate, PathArcSize largeArc, PathDirection sweep, float x, float y)
+        {
+            Rx = rx;
+            Ry = ry;
+            XAxisRotate = xAxisRotate;
+            LargeArc = largeArc;
+            Sweep = sweep;
+            X = x;
+            Y = y;
+        }
     }
 }
