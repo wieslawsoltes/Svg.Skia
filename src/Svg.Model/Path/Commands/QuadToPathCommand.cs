b0731f51002250220ad49543ed540dd0b86f5137
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:31:07 +0200

    Update QuadToPathCommand.cs

--- src/Svg.Model/Path/Commands/QuadToPathCommand.cs
+++ src/Svg.Model/Path/Commands/QuadToPathCommand.cs
@@ -6,5 +6,13 @@
         public float Y0;
         public float X1;
         public float Y1;
+
+        public QuadToPathCommand(float x0, float y0, float x1, float y1)
+        {
+            X0 = x0;
+            Y0 = y0;
+            X1 = x1;
+            Y1 = y1;
+        }
     }
 }
