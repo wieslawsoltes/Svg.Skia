f9f630e9b41a14796e731d21132e6371385ee8ce
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:30:28 +0200

    Update AddCirclePathCommand.cs

--- src/Svg.Model/Path/Commands/AddCirclePathCommand.cs
+++ src/Svg.Model/Path/Commands/AddCirclePathCommand.cs
@@ -5,5 +5,12 @@
         public float X;
         public float Y;
         public float Radius;
+
+        public AddCirclePathCommand(float x, float y, float radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
     }
 }
