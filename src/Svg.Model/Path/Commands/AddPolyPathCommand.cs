9509e7247fce4806ae30c0a20a9db56e745ebb82
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:30:35 +0200

    Update AddPolyPathCommand.cs

--- src/Svg.Model/Path/Commands/AddPolyPathCommand.cs
+++ src/Svg.Model/Path/Commands/AddPolyPathCommand.cs
@@ -4,7 +4,7 @@ namespace Svg.Model
 {
     public class AddPolyPathCommand : PathCommand
     {
-        public IList<Point> Points;
+        public IList<Point>? Points;
         public bool Close;
 
         public AddPolyPathCommand(IList<Point> points, bool close)
