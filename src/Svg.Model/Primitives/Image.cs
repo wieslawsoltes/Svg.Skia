eeddba606b6248ea41e6cc5457638446a244bfd9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:37:11 +0200

    Update Image.cs

--- src/Svg.Model/Primitives/Image.cs
+++ src/Svg.Model/Primitives/Image.cs
@@ -1,7 +1,21 @@
-﻿namespace Svg.Model
+﻿using System.IO;
+
+namespace Svg.Model
 {
     public class Image
     {
         public byte[]? Data;
+
+        public static Image FromEncodedData(Stream sourceStream)
+        {
+            using var memoryStream = new MemoryStream();
+            sourceStream.CopyTo(memoryStream);
+            var data = memoryStream.ToArray();
+
+            return new Image()
+            {
+                Data = data
+            };
+        }
     }
 }
