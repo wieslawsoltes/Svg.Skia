65cbc96907b406c814e24db40f57660c700c54a8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:29:04 +0200

    Update DistantLitDiffuseImageFilter.cs

--- src/Svg.Model/ImageFilters/DistantLitDiffuseImageFilter.cs
+++ src/Svg.Model/ImageFilters/DistantLitDiffuseImageFilter.cs
@@ -5,7 +5,7 @@
         public Point3 Direction;
         public Color LightColor;
         public float SurfaceScale;
-        public float KD;
+        public float Kd;
         public ImageFilter? Input;
         public CropRect? CropRect;
     }
