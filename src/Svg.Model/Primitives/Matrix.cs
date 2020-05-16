8691c3439748671321882a6a9871fe45073b073a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 17:03:38 +0200

    Update Matrix.cs

--- src/Svg.Model/Primitives/Matrix.cs
+++ src/Svg.Model/Primitives/Matrix.cs
@@ -14,6 +14,8 @@ namespace Svg.Model
         public float Persp1;
         public float Persp2;
 
+        internal const float DegreesToRadians = (float)Math.PI / 180.0f;
+
         public readonly static Matrix Empty;
 
         public readonly static Matrix Identity = new Matrix { ScaleX = 1, ScaleY = 1, Persp2 = 1 };
@@ -23,42 +25,155 @@ namespace Svg.Model
             return new Matrix { ScaleX = 1, ScaleY = 1, Persp2 = 1 };
         }
 
-        public static Matrix CreateRotationDegrees(float degrees)
+        public static Matrix CreateTranslation(float x, float y)
         {
-            throw new NotImplementedException(); // TODO:
-        }
+            if (x == 0 && y == 0)
+            {
+                return Identity;
+            }
 
-        public static Matrix CreateRotationDegrees(float degrees, float pivotX, float pivotY)
-        {
-            throw new NotImplementedException(); // TODO:
+            return new Matrix
+            {
+                ScaleX = 1,
+                ScaleY = 1,
+                TransX = x,
+                TransY = y,
+                Persp2 = 1,
+            };
         }
 
         public static Matrix CreateScale(float x, float y)
         {
-            throw new NotImplementedException(); // TODO:
+            if (x == 1 && y == 1)
+            {
+                return Identity;
+            }
+
+            return new Matrix
+            {
+                ScaleX = x,
+                ScaleY = y,
+                Persp2 = 1,
+            };
+        }
+
+        public static Matrix CreateScale(float x, float y, float pivotX, float pivotY)
+        {
+            if (x == 1 && y == 1)
+            {
+                return Identity;
+            }
+
+            var tx = pivotX - x * pivotX;
+            var ty = pivotY - y * pivotY;
+
+            return new Matrix
+            {
+                ScaleX = x,
+                ScaleY = y,
+                TransX = tx,
+                TransY = ty,
+                Persp2 = 1,
+            };
+        }
+
+        public static Matrix CreateRotation(float radians)
+        {
+            if (radians == 0)
+            {
+                return Identity;
+            }
+
+            var sin = (float)Math.Sin(radians);
+            var cos = (float)Math.Cos(radians);
+
+            var matrix = Identity;
+            SetSinCos(ref matrix, sin, cos);
+            return matrix;
+        }
+
+        public static Matrix CreateRotation(float radians, float pivotX, float pivotY)
+        {
+            if (radians == 0)
+            {
+                return Identity;
+            }
+
+            var sin = (float)Math.Sin(radians);
+            var cos = (float)Math.Cos(radians);
+
+            var matrix = Identity;
+            SetSinCos(ref matrix, sin, cos, pivotX, pivotY);
+            return matrix;
+        }
+
+        public static Matrix CreateRotationDegrees(float degrees)
+        {
+            if (degrees == 0)
+            {
+                return Identity;
+            }
+
+            return CreateRotation(degrees * DegreesToRadians);
+        }
+
+        public static Matrix CreateRotationDegrees(float degrees, float pivotX, float pivotY)
+        {
+            if (degrees == 0)
+            {
+                return Identity;
+            }
+
+            return CreateRotation(degrees * DegreesToRadians, pivotX, pivotY);
         }
 
         public static Matrix CreateSkew(float x, float y)
         {
-            throw new NotImplementedException(); // TODO:
+            if (x == 0 && y == 0)
+            {
+                return Identity;
+            }
+
+            return new Matrix
+            {
+                ScaleX = 1,
+                SkewX = x,
+                SkewY = y,
+                ScaleY = 1,
+                Persp2 = 1,
+            };
         }
 
-        public static Matrix CreateTranslation(float x, float y)
+        private static void SetSinCos(ref Matrix matrix, float sin, float cos)
         {
-            throw new NotImplementedException(); // TODO:
+            matrix.ScaleX = cos;
+            matrix.SkewX = -sin;
+            matrix.TransX = 0;
+            matrix.SkewY = sin;
+            matrix.ScaleY = cos;
+            matrix.TransY = 0;
+            matrix.Persp0 = 0;
+            matrix.Persp1 = 0;
+            matrix.Persp2 = 1;
         }
 
-        public Matrix(float scaleX, float skewX, float transX, float skewY, float scaleY, float transY, float persp0, float persp1, float persp2)
+        private static void SetSinCos(ref Matrix matrix, float sin, float cos, float pivotx, float pivoty)
         {
-            ScaleX = scaleX;
-            SkewX = skewX;
-            TransX = transX;
-            SkewY = skewY;
-            ScaleY = scaleY;
-            TransY = transY;
-            Persp0 = persp0;
-            Persp1 = persp1;
-            Persp2 = persp2;
+            float oneMinusCos = 1 - cos;
+            matrix.ScaleX = cos;
+            matrix.SkewX = -sin;
+            matrix.TransX = Dot(sin, pivoty, oneMinusCos, pivotx);
+            matrix.SkewY = sin;
+            matrix.ScaleY = cos;
+            matrix.TransY = Dot(-sin, pivotx, oneMinusCos, pivoty);
+            matrix.Persp0 = 0;
+            matrix.Persp1 = 0;
+            matrix.Persp2 = 1;
+        }
+
+        private static float Dot(float a, float b, float c, float d)
+        {
+            return a * b + c * d;
         }
 
         private static float MulAddMul(float a, float b, float c, float d)
@@ -66,7 +181,7 @@ namespace Svg.Model
             return (float)((double)a * (double)b + (double)c * (double)d);
         }
 
-        private static Matrix Concat(Matrix a, Matrix b) 
+        private static Matrix Concat(Matrix a, Matrix b)
         {
             return new Matrix
             {
@@ -82,6 +197,19 @@ namespace Svg.Model
             };
         }
 
+        public Matrix(float scaleX, float skewX, float transX, float skewY, float scaleY, float transY, float persp0, float persp1, float persp2)
+        {
+            ScaleX = scaleX;
+            SkewX = skewX;
+            TransX = transX;
+            SkewY = skewY;
+            ScaleY = scaleY;
+            TransY = transY;
+            Persp0 = persp0;
+            Persp1 = persp1;
+            Persp2 = persp2;
+        }
+
         public readonly Matrix PreConcat(Matrix matrix)
         {
             return Concat(this, matrix);
@@ -94,7 +222,7 @@ namespace Svg.Model
 
         public readonly Rect MapRect(Rect source)
         {
-            throw new NotImplementedException(); // TODO:
+            return source; // TODO:
         }
     }
 }
