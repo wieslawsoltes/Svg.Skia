5bbe8e2e1d1b4b6e6b6f4d3378f402c840c29d99
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:29:07 +0200

    Update DistantLitSpecularImageFilter.cs

--- src/Svg.Model/ImageFilters/DistantLitSpecularImageFilter.cs
+++ src/Svg.Model/ImageFilters/DistantLitSpecularImageFilter.cs
@@ -5,7 +5,7 @@
         public Point3 Direction;
         public Color LightColor;
         public float SurfaceScale;
-        public float KS;
+        public float Ks;
         public float Shininess;
         public ImageFilter? Input;
         public CropRect? CropRect;
