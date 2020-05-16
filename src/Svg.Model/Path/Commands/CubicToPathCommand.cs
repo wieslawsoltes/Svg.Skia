9c1a762f02d06ccc12073c1f9e210c77b516427a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-16 13:30:55 +0200

    Update CubicToPathCommand.cs

--- src/Svg.Model/Path/Commands/CubicToPathCommand.cs
+++ src/Svg.Model/Path/Commands/CubicToPathCommand.cs
@@ -8,5 +8,15 @@
         public float Y1;
         public float X2;
         public float Y2;
+
+        public CubicToPathCommand(float x0, float y0, float x1, float y1, float x2, float y2)
+        {
+            X0 = x0;
+            Y0 = y0;
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
     }
 }
