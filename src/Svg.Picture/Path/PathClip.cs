75236d123abb249d92ce225f7c856e6502343fb9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-24 12:45:30 +0200

    Create PathClip.cs

---
+++ src/Svg.Picture/Path/PathClip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Svg.Picture
+{
+    public class PathClip : IDisposable
+    {
+        public Path? Path;
+        public Matrix? Transform;
+        public PathOp Op;
+
+        public void Dispose()
+        {
+        }
+    }
+}
