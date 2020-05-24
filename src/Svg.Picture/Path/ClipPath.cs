cc40ec1d4a728ca5e3bcbbe461cc0c657bae96a5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-24 12:45:38 +0200

    Create ClipPath.cs

---
+++ src/Svg.Picture/Path/ClipPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Svg.Picture
+{
+    public class ClipPath : IDisposable
+    {
+        public IList<PathClip>? Clips;
+        public Matrix? Transform;
+
+        public bool IsEmpty => Clips == null || Clips.Count == 0;
+
+        public ClipPath()
+        {
+            Clips = new List<PathClip>();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
