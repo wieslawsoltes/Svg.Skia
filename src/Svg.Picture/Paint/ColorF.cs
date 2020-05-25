a50861606938e68e3b8bdc1f439da9c629d74af4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-25 09:49:36 +0200

    Create ColorF.cs

---
+++ src/Svg.Picture/Paint/ColorF.cs
@@ -0,0 +1,20 @@
+﻿namespace Svg.Picture
+{
+    public struct ColorF
+    {
+        public float Red { get; set; }
+        public float Green { get; set; }
+        public float Blue { get; set; }
+        public float Alpha { get; set; }
+
+        public static readonly ColorF Empty;
+
+        public ColorF(float red, float green, float blue, float alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+    }
+}
