8cf81a6a48258e8e5a2542814d64dd4c6e4bb41d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-06-17 08:30:02 +0200

    Move class

--- src/Svg.Skia/Svg/SvgExtensions.cs
+++ src/Svg.Skia/Svg/SvgExtensions.cs
@@ -5176,46 +5176,6 @@ namespace Svg.Skia
         }
     }
 
-#if USE_PICTURE
-    internal sealed class CompositeDisposable : IDisposable
-    {
-        public CompositeDisposable()
-        {
-        }
-
-        public void Add(object disposable)
-        {
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-#else
-    internal sealed class CompositeDisposable : IDisposable
-    {
-        private readonly List<IDisposable> _disposables;
-
-        public CompositeDisposable()
-        {
-            _disposables = new List<IDisposable>();
-        }
-
-        public void Add(IDisposable disposable)
-        {
-            _disposables.Add(disposable);
-        }
-
-        public void Dispose()
-        {
-            foreach (var disposable in _disposables)
-            {
-                disposable.Dispose();
-            }
-        }
-    }
-#endif
-
     [Flags]
     internal enum Attributes
     {
