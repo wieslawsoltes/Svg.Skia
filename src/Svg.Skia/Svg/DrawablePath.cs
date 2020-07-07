d60400079aae64f3ef6e315b96bce8ae4c259d74
Jason Nelson <jason@carbonmade.com>
2020-07-07 15:33:03 -0700

    Breakout DrawablePath

---
+++ src/Svg.Skia/Svg/DrawablePath.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+#if !USE_PICTURE
+using SkiaSharp;
+using CropRect = SkiaSharp.SKImageFilter.CropRect;
+#endif
+#if USE_PICTURE
+using SKCanvas = Svg.Picture.Canvas;
+using SKPath = Svg.Picture.Path;
+#endif
+
+namespace Svg.Skia
+{
+    internal abstract class DrawablePath : DrawableBase
+    {
+        public SKPath? Path;
+
+        public List<DrawableBase>? MarkerDrawables;
+
+        protected DrawablePath()
+            : base()
+        {
+        }
+
+        public override void OnDraw(SKCanvas canvas, Attributes ignoreAttributes, DrawableBase? until)
+        {
+            if (until != null && this == until)
+            {
+                return;
+            }
+
+            if (Fill != null && Path != null)
+            {
+                canvas.DrawPath(Path, Fill);
+            }
+
+            if (Stroke != null && Path != null)
+            {
+                canvas.DrawPath(Path, Stroke);
+            }
+
+            if (MarkerDrawables != null)
+            {
+                foreach (var drawable in MarkerDrawables)
+                {
+                    drawable.Draw(canvas, ignoreAttributes, until);
+                }
+            }
+        }
+
+        public override void PostProcess()
+        {
+            base.PostProcess();
+
+            if (MarkerDrawables != null)
+            {
+                foreach (var drawable in MarkerDrawables)
+                {
+                    drawable.PostProcess();
+                }
+            }
+        }
+    }
+}
--- src/Svg.Skia/Svg/SvgExtensions.cs
+++ src/Svg.Skia/Svg/SvgExtensions.cs
@@ -5140,57 +5140,6 @@ namespace Svg.Skia
         void Draw(SKCanvas canvas, Attributes ignoreAttributes, DrawableBase? until);
     }
 
-    internal abstract class DrawablePath : DrawableBase
-    {
-        public SKPath? Path;
-
-        public List<DrawableBase>? MarkerDrawables;
-
-        protected DrawablePath()
-            : base()
-        {
-        }
-
-        public override void OnDraw(SKCanvas canvas, Attributes ignoreAttributes, DrawableBase? until)
-        {
-            if (until != null && this == until)
-            {
-                return;
-            }
-
-            if (Fill != null && Path != null)
-            {
-                canvas.DrawPath(Path, Fill);
-            }
-
-            if (Stroke != null && Path != null)
-            {
-                canvas.DrawPath(Path, Stroke);
-            }
-
-            if (MarkerDrawables != null)
-            {
-                foreach (var drawable in MarkerDrawables)
-                {
-                    drawable.Draw(canvas, ignoreAttributes, until);
-                }
-            }
-        }
-
-        public override void PostProcess()
-        {
-            base.PostProcess();
-
-            if (MarkerDrawables != null)
-            {
-                foreach (var drawable in MarkerDrawables)
-                {
-                    drawable.PostProcess();
-                }
-            }
-        }
-    }
-
     internal abstract class DrawableContainer : DrawableBase
     {
         public readonly List<DrawableBase> ChildrenDrawables;
