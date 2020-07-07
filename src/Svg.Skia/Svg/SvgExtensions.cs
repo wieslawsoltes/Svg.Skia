09845a25b91ee39596899e8152b92a02055b6aef
Jason Nelson <jason@carbonmade.com>
2020-07-07 15:23:13 -0700

    Breakout supported svg features

--- src/Svg.Skia/Svg/SvgExtensions.cs
+++ src/Svg.Skia/Svg/SvgExtensions.cs
@@ -61,10 +61,6 @@ namespace Svg.Skia
 {
     internal static class SvgExtensions
     {
-        private static readonly char[] s_space_tab = new char[2] { ' ', '\t' };
-
-        private static readonly char[] s_comma = new char[] { ',' };
-
         [Flags]
         internal enum PathPointType : byte
         {
@@ -78,55 +74,9 @@ namespace Svg.Skia
             CloseSubpath = 0x80
         }
 
-        public static HashSet<string> s_supportedFeatures = new HashSet<string>()
-        {
-            "http://www.w3.org/TR/SVG11/feature#SVG",
-            "http://www.w3.org/TR/SVG11/feature#SVGDOM",
-            "http://www.w3.org/TR/SVG11/feature#SVG-static",
-            "http://www.w3.org/TR/SVG11/feature#SVGDOM-static",
-            "http://www.w3.org/TR/SVG11/feature#SVG-animation",
-            "http://www.w3.org/TR/SVG11/feature#SVGDOM-animation",
-            "http://www.w3.org/TR/SVG11/feature#SVG-dynamic",
-            "http://www.w3.org/TR/SVG11/feature#SVGDOM-dynamic",
-            "http://www.w3.org/TR/SVG11/feature#CoreAttribute",
-            "http://www.w3.org/TR/SVG11/feature#Structure",
-            "http://www.w3.org/TR/SVG11/feature#BasicStructure",
-            "http://www.w3.org/TR/SVG11/feature#ContainerAttribute",
-            "http://www.w3.org/TR/SVG11/feature#ConditionalProcessing",
-            "http://www.w3.org/TR/SVG11/feature#Image",
-            "http://www.w3.org/TR/SVG11/feature#Style",
-            "http://www.w3.org/TR/SVG11/feature#ViewportAttribute",
-            "http://www.w3.org/TR/SVG11/feature#Shape",
-            "http://www.w3.org/TR/SVG11/feature#Text",
-            "http://www.w3.org/TR/SVG11/feature#BasicText",
-            "http://www.w3.org/TR/SVG11/feature#PaintAttribute",
-            "http://www.w3.org/TR/SVG11/feature#BasicPaintAttribute",
-            "http://www.w3.org/TR/SVG11/feature#OpacityAttribute",
-            "http://www.w3.org/TR/SVG11/feature#GraphicsAttribute",
-            "http://www.w3.org/TR/SVG11/feature#BasicGraphicsAttribute",
-            "http://www.w3.org/TR/SVG11/feature#Marker",
-            "http://www.w3.org/TR/SVG11/feature#ColorProfile",
-            "http://www.w3.org/TR/SVG11/feature#Gradient",
-            "http://www.w3.org/TR/SVG11/feature#Pattern",
-            "http://www.w3.org/TR/SVG11/feature#Clip",
-            "http://www.w3.org/TR/SVG11/feature#BasicClip",
-            "http://www.w3.org/TR/SVG11/feature#Mask",
-            "http://www.w3.org/TR/SVG11/feature#Filter",
-            "http://www.w3.org/TR/SVG11/feature#BasicFilter",
-            "http://www.w3.org/TR/SVG11/feature#DocumentEventsAttribute",
-            "http://www.w3.org/TR/SVG11/feature#GraphicalEventsAttribute",
-            "http://www.w3.org/TR/SVG11/feature#AnimationEventsAttribute",
-            "http://www.w3.org/TR/SVG11/feature#Cursor",
-            "http://www.w3.org/TR/SVG11/feature#Hyperlinking",
-            "http://www.w3.org/TR/SVG11/feature#XlinkAttribute",
-            "http://www.w3.org/TR/SVG11/feature#ExternalResourcesRequired",
-            "http://www.w3.org/TR/SVG11/feature#View",
-            "http://www.w3.org/TR/SVG11/feature#Script",
-            "http://www.w3.org/TR/SVG11/feature#Animation",
-            "http://www.w3.org/TR/SVG11/feature#Font",
-            "http://www.w3.org/TR/SVG11/feature#BasicFont",
-            "http://www.w3.org/TR/SVG11/feature#Extensibility"
-        };
+        private static readonly char[] s_space_tab = new char[2] { ' ', '\t' };
+
+        private static readonly char[] s_comma = new char[] { ',' };
 
         public static HashSet<string> s_supportedExtensions = new HashSet<string>()
         {
@@ -502,6 +452,8 @@ namespace Svg.Skia
             return null;
         }
 
+        private static readonly char[] s_tab_space = { ' ', '\t' };
+
         public static bool HasRequiredFeatures(this SvgElement svgElement)
         {
             bool hasRequiredFeatures = true;
@@ -514,12 +466,12 @@ namespace Svg.Skia
                 }
                 else
                 {
-                    var features = requiredFeaturesString.Trim().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    var features = requiredFeaturesString.Trim().Split(s_tab_space, StringSplitOptions.RemoveEmptyEntries);
                     if (features.Length > 0)
                     {
                         foreach (var feature in features)
                         {
-                            if (!s_supportedFeatures.Contains(feature))
+                            if (!SvgFeatures.Contains(feature))
                             {
                                 hasRequiredFeatures = false;
                                 break;
---
+++ src/Svg.Skia/Svg/SvgFeatures.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Svg.Skia
+{
+    internal static class SvgFeatures
+    {
+        private static readonly HashSet<string> s_supportedFeatures = new HashSet<string>
+        {
+            "http://www.w3.org/TR/SVG11/feature#SVG",
+            "http://www.w3.org/TR/SVG11/feature#SVGDOM",
+            "http://www.w3.org/TR/SVG11/feature#SVG-static",
+            "http://www.w3.org/TR/SVG11/feature#SVGDOM-static",
+            "http://www.w3.org/TR/SVG11/feature#SVG-animation",
+            "http://www.w3.org/TR/SVG11/feature#SVGDOM-animation",
+            "http://www.w3.org/TR/SVG11/feature#SVG-dynamic",
+            "http://www.w3.org/TR/SVG11/feature#SVGDOM-dynamic",
+            "http://www.w3.org/TR/SVG11/feature#CoreAttribute",
+            "http://www.w3.org/TR/SVG11/feature#Structure",
+            "http://www.w3.org/TR/SVG11/feature#BasicStructure",
+            "http://www.w3.org/TR/SVG11/feature#ContainerAttribute",
+            "http://www.w3.org/TR/SVG11/feature#ConditionalProcessing",
+            "http://www.w3.org/TR/SVG11/feature#Image",
+            "http://www.w3.org/TR/SVG11/feature#Style",
+            "http://www.w3.org/TR/SVG11/feature#ViewportAttribute",
+            "http://www.w3.org/TR/SVG11/feature#Shape",
+            "http://www.w3.org/TR/SVG11/feature#Text",
+            "http://www.w3.org/TR/SVG11/feature#BasicText",
+            "http://www.w3.org/TR/SVG11/feature#PaintAttribute",
+            "http://www.w3.org/TR/SVG11/feature#BasicPaintAttribute",
+            "http://www.w3.org/TR/SVG11/feature#OpacityAttribute",
+            "http://www.w3.org/TR/SVG11/feature#GraphicsAttribute",
+            "http://www.w3.org/TR/SVG11/feature#BasicGraphicsAttribute",
+            "http://www.w3.org/TR/SVG11/feature#Marker",
+            "http://www.w3.org/TR/SVG11/feature#ColorProfile",
+            "http://www.w3.org/TR/SVG11/feature#Gradient",
+            "http://www.w3.org/TR/SVG11/feature#Pattern",
+            "http://www.w3.org/TR/SVG11/feature#Clip",
+            "http://www.w3.org/TR/SVG11/feature#BasicClip",
+            "http://www.w3.org/TR/SVG11/feature#Mask",
+            "http://www.w3.org/TR/SVG11/feature#Filter",
+            "http://www.w3.org/TR/SVG11/feature#BasicFilter",
+            "http://www.w3.org/TR/SVG11/feature#DocumentEventsAttribute",
+            "http://www.w3.org/TR/SVG11/feature#GraphicalEventsAttribute",
+            "http://www.w3.org/TR/SVG11/feature#AnimationEventsAttribute",
+            "http://www.w3.org/TR/SVG11/feature#Cursor",
+            "http://www.w3.org/TR/SVG11/feature#Hyperlinking",
+            "http://www.w3.org/TR/SVG11/feature#XlinkAttribute",
+            "http://www.w3.org/TR/SVG11/feature#ExternalResourcesRequired",
+            "http://www.w3.org/TR/SVG11/feature#View",
+            "http://www.w3.org/TR/SVG11/feature#Script",
+            "http://www.w3.org/TR/SVG11/feature#Animation",
+            "http://www.w3.org/TR/SVG11/feature#Font",
+            "http://www.w3.org/TR/SVG11/feature#BasicFont",
+            "http://www.w3.org/TR/SVG11/feature#Extensibility"
+        };
+
+
+        public static bool Contains(string name)
+        {
+            return s_supportedFeatures.Contains(name);
+        }
+    }
+}
