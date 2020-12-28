4ef2357ee49d94bc9b1e2e685edeb45377d05fc7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-12-28 22:27:04 +0100

    Update IPictureSource.cs

--- src/Svg.Model/IPictureSource.cs
+++ src/Svg.Model/IPictureSource.cs
@@ -5,7 +5,6 @@ namespace Svg.Model
     internal interface IPictureSource
     {
         void OnDraw(Canvas canvas, Attributes ignoreAttributes, DrawableBase? until);
-
         void Draw(Canvas canvas, Attributes ignoreAttributes, DrawableBase? until);
     }
 }
