fc96edf22a4741bba2ded18aad80e5d2e122feb5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-21 09:13:11 +0200

    Update Program.cs

--- samples/TestApp/Program.cs
+++ samples/TestApp/Program.cs
@@ -1,18 +1,29 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.ReactiveUI;
+using Avalonia.Svg.Skia;
 
 namespace TestApp
 {
     class Program
     {
+        [STAThread]
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
         public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
+        {
+            GC.KeepAlive(typeof(SvgImageExtension).Assembly);
+            GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+
+            return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new X11PlatformOptions
+                {
+                })
                 .LogToTrace()
                 .UseSkia()
                 .UseReactiveUI();
+        }
     }
 }
