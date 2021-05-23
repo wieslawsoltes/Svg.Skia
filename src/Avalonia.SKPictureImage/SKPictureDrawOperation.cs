12379680da879006c077849c543368b3d2718e61
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-23 19:38:25 +0200

    Move SKPictureDrawOperation

---
+++ src/Avalonia.SKPictureImage/SKPictureDrawOperation.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using SkiaSharp;
+
+namespace Avalonia.SKPictureImage
+{
+    public class SKPictureDrawOperation : ICustomDrawOperation
+    {
+        private readonly SKPicture? _picture;
+
+        public SKPictureDrawOperation(Rect bounds, SKPicture? picture)
+        {
+            _picture = picture;
+            Bounds = bounds;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Rect Bounds { get; }
+
+        public bool HitTest(Point p) => false;
+
+        public bool Equals(ICustomDrawOperation? other) => false;
+
+        public void Render(IDrawingContextImpl context)
+        {
+            var canvas = (context as ISkiaDrawingContextImpl)?.SkCanvas;
+            if (canvas is { } && _picture is { })
+            {
+                canvas.DrawPicture(_picture);
+            }
+        }
+    }
+}
--- src/Avalonia.SKPictureImage/SKPictureImage.cs
+++ src/Avalonia.SKPictureImage/SKPictureImage.cs
@@ -2,44 +2,11 @@
 using System;
 using Avalonia.Media;
 using Avalonia.Metadata;
-using Avalonia.Platform;
-using Avalonia.Rendering.SceneGraph;
-using Avalonia.Skia;
 using Avalonia.Visuals.Media.Imaging;
 using SkiaSharp;
 
 namespace Avalonia.SKPictureImage
 {
-    internal class SKPictureDrawOperation : ICustomDrawOperation
-    {
-        private readonly SKPicture _picture;
-
-        public SKPictureDrawOperation(Rect bounds, SKPicture picture)
-        {
-            _picture = picture;
-            Bounds = bounds;
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public Rect Bounds { get; }
-
-        public bool HitTest(Point p) => false;
-
-        public bool Equals(ICustomDrawOperation other) => false;
-
-        public void Render(IDrawingContextImpl context)
-        {
-            var canvas = (context as ISkiaDrawingContextImpl)?.SkCanvas;
-            if (canvas is { } && _picture is { })
-            {
-                canvas.DrawPicture(_picture);
-            }
-        }
-    }
-
     /// <summary>
     /// An <see cref="IImage"/> that uses a <see cref="SKPicture"/> for content.
     /// </summary>
