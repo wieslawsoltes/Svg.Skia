b2e09066b37f90727a3d41b39da51cf77e1570b2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-26 20:12:51 +0200

    Add filter support

--- samples/TestApp/ViewModels/MainWindowViewModel.cs
+++ samples/TestApp/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
 
 namespace TestApp.ViewModels
@@ -7,7 +11,9 @@ namespace TestApp.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private FileItemViewModel? _selectedItem;
-        private IList<FileItemViewModel>? _items;
+        private ObservableCollection<FileItemViewModel>? _items;
+        private string? _itemQuery;
+        private ReadOnlyObservableCollection<FileItemViewModel>? _filteredItems;
 
         public FileItemViewModel? SelectedItem
         {
@@ -15,15 +21,63 @@ namespace TestApp.ViewModels
             set => this.RaiseAndSetIfChanged(ref _selectedItem, value);
         }
 
-        public IList<FileItemViewModel>? Items
+        public ObservableCollection<FileItemViewModel>? Items
         {
             get => _items;
             set => this.RaiseAndSetIfChanged(ref _items, value);
         }
 
+        public string? ItemQuery
+        {
+            get => _itemQuery;
+            set => this.RaiseAndSetIfChanged(ref _itemQuery, value);
+        }
+
+        public ReadOnlyObservableCollection<FileItemViewModel>? FilteredItems
+        {
+            get => _filteredItems;
+            set => this.RaiseAndSetIfChanged(ref _filteredItems, value);
+        }
+
+        public ICommand ResetQueryCommand { get; }
+        
         public MainWindowViewModel()
         {
-            Items = new ObservableCollection<FileItemViewModel>();
+            _items = new ObservableCollection<FileItemViewModel>();
+
+            var queryFilter = this.WhenValueChanged(t => t.ItemQuery)
+                .Throttle(TimeSpan.FromMilliseconds(100))
+                .Select(ItemQueryFilter)
+                .DistinctUntilChanged();
+
+            _items
+                .ToObservableChangeSet()
+                .Filter(queryFilter)
+                .Sort(SortExpressionComparer<FileItemViewModel>.Ascending(x => x.Name))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Bind(out _filteredItems)
+                .AsObservableList();
+
+            var resetQueryCanExecute = this.WhenAnyValue(x => x.ItemQuery)
+                .Select(x => !string.IsNullOrWhiteSpace(x))
+                .ObserveOn(RxApp.MainThreadScheduler);
+
+            ResetQueryCommand = ReactiveCommand.Create(
+                () => ItemQuery = "", 
+                resetQueryCanExecute);
+        }
+
+        private Func<FileItemViewModel, bool> ItemQueryFilter(string? searchQuery)
+        {
+            return item =>
+            {
+                if (!string.IsNullOrWhiteSpace(searchQuery))
+                {
+                    return item.Name.Contains(searchQuery, StringComparison.OrdinalIgnoreCase);
+                }
+
+                return true;
+            };
         }
     }
 }
