90949441862bdc4bc5ca2772448ddbaf9a1a435c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-08 11:30:23 +0200

    Add SKPictureControl

---
+++ src/Avalonia.SKPictureImage/SKPictureControl.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Metadata;
+
+namespace Avalonia.SKPictureImage
+{
+    /// <summary>
+    /// SKPicture control.
+    /// </summary>
+    public class SKPictureControl : Control, IAffectsRender
+    {
+        /// <summary>
+        /// Defines the <see cref="Picture"/> property.
+        /// </summary>
+        public static readonly StyledProperty<SkiaSharp.SKPicture?> PictureProperty =
+            AvaloniaProperty.Register<SKPictureControl, SkiaSharp.SKPicture?>(nameof(Picture));
+
+        /// <summary>
+        /// Defines the <see cref="Stretch"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Stretch> StretchProperty =
+            AvaloniaProperty.Register<SKPictureControl, Stretch>(nameof(Stretch), Stretch.Uniform);
+
+        /// <summary>
+        /// Defines the <see cref="StretchDirection"/> property.
+        /// </summary>
+        public static readonly StyledProperty<StretchDirection> StretchDirectionProperty =
+            AvaloniaProperty.Register<SKPictureControl, StretchDirection>(
+                nameof(StretchDirection),
+                StretchDirection.Both);
+
+        /// <inheritdoc/>
+        public event EventHandler? Invalidated;
+
+        /// <summary>
+        /// Gets or sets the <see cref="SkiaSharp.SKPicture"/> picture.
+        /// </summary>
+        [Content]
+        public SkiaSharp.SKPicture? Picture
+        {
+            get => GetValue(PictureProperty);
+            set => SetValue(PictureProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value controlling how the image will be stretched.
+        /// </summary>
+        public Stretch Stretch
+        {
+            get { return GetValue(StretchProperty); }
+            set { SetValue(StretchProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value controlling in what direction the image will be stretched.
+        /// </summary>
+        public StretchDirection StretchDirection
+        {
+            get { return GetValue(StretchDirectionProperty); }
+            set { SetValue(StretchDirectionProperty, value); }
+        }
+
+        static SKPictureControl()
+        {
+            AffectsRender<SKPictureControl>(PictureProperty, StretchProperty, StretchDirectionProperty);
+            AffectsMeasure<SKPictureControl>(PictureProperty, StretchProperty, StretchDirectionProperty);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var picture = Picture;
+            if (picture is null)
+            {
+                return new Size();
+            }
+
+            var sourceSize = new Size(picture.CullRect.Width, picture.CullRect.Height);
+            return Stretch.CalculateSize(availableSize, sourceSize, StretchDirection);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var picture = Picture;
+            if (picture is null)
+            {
+                return new Size();
+            }
+
+            var sourceSize = new Size(picture.CullRect.Width, picture.CullRect.Height);
+            return Stretch.CalculateSize(finalSize, sourceSize);
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            var picture = Picture;
+            if (picture is null)
+            {
+                return;
+            }
+
+            var viewPort = new Rect(Bounds.Size);
+            var sourceSize = new Size(picture.CullRect.Width, picture.CullRect.Height);
+            if (sourceSize.Width <= 0 || sourceSize.Height <= 0)
+            {
+                return;
+            }
+
+            var scale = Stretch.CalculateScaling(Bounds.Size, sourceSize, StretchDirection);
+            var scaledSize = sourceSize * scale;
+            var destRect = viewPort
+                .CenterRect(new Rect(scaledSize))
+                .Intersect(viewPort);
+            var sourceRect = new Rect(sourceSize)
+                .CenterRect(new Rect(destRect.Size / scale));
+
+            var bounds = picture.CullRect;
+            var scaleMatrix = Matrix.CreateScale(
+                destRect.Width / sourceRect.Width,
+                destRect.Height / sourceRect.Height);
+            var translateMatrix = Matrix.CreateTranslation(
+                -sourceRect.X + destRect.X - bounds.Top,
+                -sourceRect.Y + destRect.Y - bounds.Left);
+
+            using (context.PushClip(destRect))
+            using (context.PushPreTransform(translateMatrix * scaleMatrix))
+            {
+                context.Custom(
+                    new SKPictureDrawOperation(
+                        new Rect(0, 0, bounds.Width, bounds.Height),
+                        picture));
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == PictureProperty)
+            {
+                RaiseInvalidated(EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="Invalidated"/> event.
+        /// </summary>
+        /// <param name="e">The event args.</param>
+        protected void RaiseInvalidated(EventArgs e) => Invalidated?.Invoke(this, e);
+    }
+}
