3beee9e0f15a559b28a765aa61b362889fa64e19
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-07 09:57:54 +0200

    Use new file-scoped namespace declaration

--- tests/Avalonia.Svg.Skia.UnitTests/SvgImageTests.cs
+++ tests/Avalonia.Svg.Skia.UnitTests/SvgImageTests.cs
@@ -5,25 +5,24 @@ using Avalonia.Shared.PlatformSupport;
 using Avalonia.Svg.Skia;
 using Xunit;
 
-namespace Avalonia.Svg.Skia.UnitTests
+namespace Avalonia.Svg.Skia.UnitTests;
+
+public class SvgImageTests
 {
-    public class SvgImageTests
+    [Fact]
+    public void SvgImage_Load()
     {
-        [Fact]
-        public void SvgImage_Load()
-        {
-            var uri = new Uri($"avares://Avalonia.Svg.Skia.UnitTests/Assets/Icon.svg");
-            var assetLoader = new AssetLoader(); // AvaloniaLocator.Current.GetService<IAssetLoader>()
+        var uri = new Uri($"avares://Avalonia.Svg.Skia.UnitTests/Assets/Icon.svg");
+        var assetLoader = new AssetLoader(); // AvaloniaLocator.Current.GetService<IAssetLoader>()
 
-            var svgFile = assetLoader.Open(uri);
-            Assert.NotNull(svgFile);
+        var svgFile = assetLoader.Open(uri);
+        Assert.NotNull(svgFile);
 
-            var svgSource = new SvgSource();
-            var picture = svgSource.Load(svgFile);
-            Assert.NotNull(picture);
+        var svgSource = new SvgSource();
+        var picture = svgSource.Load(svgFile);
+        Assert.NotNull(picture);
 
-            var svgImage = new SvgImage() { Source = svgSource };
-            Assert.NotNull(svgImage);
-        }
+        var svgImage = new SvgImage() { Source = svgSource };
+        Assert.NotNull(svgImage);
     }
 }
