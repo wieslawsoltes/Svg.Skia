13977919d72e758a508238a1c85d690e4f5012eb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-25 21:53:12 +0200

    Update tests

--- tests/Svg.Skia.UnitTests/SKSvgTests.cs
+++ tests/Svg.Skia.UnitTests/SKSvgTests.cs
@@ -1,14 +1,84 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Svg.Skia.UnitTests;
 
 public class SKSvgTests
 {
-    private static string GetPath(string name) => Path.Combine("..", "..", "..", "..", "Tests", name);
+    private string GetPath(string name) => Path.Combine("..", "..", "..", "..", "Tests", name);
 
+    private void CompareImages(string name, string actualPath, string expectedPath)
+    {
+        using var expected = Image.Load<Rgba32>(expectedPath);
+        using var actual = Image.Load<Rgba32>(actualPath);
+        var immediateError = CompareImages(actual, expected);
+
+        _output.WriteLine($"[{name}] {immediateError}");
+
+        if (immediateError > 0.028) // 0.022
+        {
+            Assert.True(false, actualPath + ": Error = " + immediateError);
+        }
+    }
+
+    private double CompareImages(Image<Rgba32> actual, Image<Rgba32> expected)
+    {
+        if (actual.Width != expected.Width || actual.Height != expected.Height)
+        {
+            throw new ArgumentException("Images have different resolutions");
+        }
+
+        var quantity = actual.Width * actual.Height;
+        double squaresError = 0;
+
+        const double scale = 1 / 255d;
+        
+        for (var x = 0; x < actual.Width; x++)
+        {
+            double localError = 0;
+            
+            for (var y = 0; y < actual.Height; y++)
+            {
+                var expectedAlpha = expected[x, y].A * scale;
+                var actualAlpha = actual[x, y].A * scale;
+                
+                var r = scale * (expectedAlpha * expected[x, y].R - actualAlpha * actual[x, y].R);
+                var g = scale * (expectedAlpha * expected[x, y].G - actualAlpha * actual[x, y].G);
+                var b = scale * (expectedAlpha * expected[x, y].B - actualAlpha * actual[x, y].B);
+                var a = expectedAlpha - actualAlpha;
+
+                var error = r * r + g * g + b * b + a * a;
+
+                localError += error;
+            }
+
+            squaresError += localError;
+        }
+
+        var meanSquaresError = squaresError / quantity;
+
+        const int channelCount = 4;
+        
+        meanSquaresError = meanSquaresError / channelCount;
+        
+        return Math.Sqrt(meanSquaresError);
+    }
+
+    private readonly ITestOutputHelper _output;
+
+    public SKSvgTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+    
     [Theory]
     [InlineData("Sign in")]
+    [InlineData("__AJ_Digital_Camera")]
     public void Test(string name)
     {
         var inSvgPath = GetPath($"{name}.svg");
@@ -19,10 +89,8 @@ public class SKSvgTests
         using var _ = svg.Load(inSvgPath);
         svg.Save(actualPng, SkiaSharp.SKColors.Transparent);
 
-        using var expected = SkiaSharp.SKBitmap.Decode(expectedPng);
-        using var actual = SkiaSharp.SKBitmap.Decode(actualPng);
+        CompareImages(name, actualPng, expectedPng);
 
-        Assert.Equal(expected.Info, actual.Info);
-        Assert.Equal(expected.Bytes, actual.Bytes);
+        File.Delete(actualPng);
     }
 }
