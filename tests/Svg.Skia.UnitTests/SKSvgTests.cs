2c9d4145d964680aa8cab1325959161f70e1f969
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-26 14:57:01 +0200

    Update SKSvgTests.cs

--- tests/Svg.Skia.UnitTests/SKSvgTests.cs
+++ tests/Svg.Skia.UnitTests/SKSvgTests.cs
@@ -1,79 +1,18 @@
-﻿using System;
-using System.IO;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+﻿using System.IO;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Svg.Skia.UnitTests;
 
 public class SKSvgTests
 {
-    private readonly ITestOutputHelper _output;
+    private string GetSvgPath(string name) 
+        => Path.Combine("..", "..", "..", "..", "Tests", name);
 
-    private string GetPath(string name) => Path.Combine("..", "..", "..", "..", "Tests", name);
+    private string GetExpectedPngPath(string name) 
+        => Path.Combine("..", "..", "..", "..", "Tests", name);
 
-    private void CompareImages(string name, string actualPath, string expectedPath, double errorThreshold)
-    {
-        using var expected = Image.Load<Rgba32>(expectedPath);
-        using var actual = Image.Load<Rgba32>(actualPath);
-        var immediateError = CompareImages(actual, expected);
-
-        _output.WriteLine($"[{name}] {immediateError}");
-
-        if (immediateError > errorThreshold)
-        {
-            Assert.True(false, name + ": Error = " + immediateError);
-        }
-    }
-
-    private double CompareImages(Image<Rgba32> actual, Image<Rgba32> expected)
-    {
-        if (actual.Width != expected.Width || actual.Height != expected.Height)
-        {
-            throw new ArgumentException("Images have different resolutions");
-        }
-
-        var quantity = actual.Width * actual.Height;
-        double squaresError = 0;
-
-        const double scale = 1 / 255d;
-        
-        for (var x = 0; x < actual.Width; x++)
-        {
-            double localError = 0;
-            
-            for (var y = 0; y < actual.Height; y++)
-            {
-                var expectedAlpha = expected[x, y].A * scale;
-                var actualAlpha = actual[x, y].A * scale;
-                
-                var r = scale * (expectedAlpha * expected[x, y].R - actualAlpha * actual[x, y].R);
-                var g = scale * (expectedAlpha * expected[x, y].G - actualAlpha * actual[x, y].G);
-                var b = scale * (expectedAlpha * expected[x, y].B - actualAlpha * actual[x, y].B);
-                var a = expectedAlpha - actualAlpha;
-
-                var error = r * r + g * g + b * b + a * a;
-
-                localError += error;
-            }
-
-            squaresError += localError;
-        }
-
-        var meanSquaresError = squaresError / quantity;
-
-        const int channelCount = 4;
-        
-        meanSquaresError = meanSquaresError / channelCount;
-        
-        return Math.Sqrt(meanSquaresError);
-    }
-
-    public SKSvgTests(ITestOutputHelper output)
-    {
-        _output = output;
-    }
+    private string GetActualPngPath(string name) 
+        => Path.Combine("..", "..", "..", "..", "Tests", name);
 
     [WindowsTheory]
     [InlineData("Sign in", 0.022)]
@@ -82,15 +21,15 @@ public class SKSvgTests
     [InlineData("__tiger", 0.055)]
     public void Test(string name, double errorThreshold)
     {
-        var inSvgPath = GetPath($"{name}.svg");
-        var expectedPng = GetPath($"{name}.png");
-        var actualPng = GetPath($"{name} (Actual).png");
+        var svgPath = GetSvgPath($"{name}.svg");
+        var expectedPng = GetExpectedPngPath($"{name}.png");
+        var actualPng = GetActualPngPath($"{name} (Actual).png");
 
         var svg = new SKSvg();
-        using var _ = svg.Load(inSvgPath);
+        using var _ = svg.Load(svgPath);
         svg.Save(actualPng, SkiaSharp.SKColors.Transparent);
 
-        CompareImages(name, actualPng, expectedPng, errorThreshold);
+        ImageHelper.CompareImages(name, actualPng, expectedPng, errorThreshold);
 
         File.Delete(actualPng);
     }
