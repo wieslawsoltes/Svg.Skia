8f4ea64615b9f35abe53c35b94549a6ae6379347
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-26 15:27:34 +0200

    Make param nullable

--- src/Svg.Skia/SkiaAssetLoader.cs
+++ src/Svg.Skia/SkiaAssetLoader.cs
@@ -15,11 +15,14 @@ public class SkiaAssetLoader : Model.IAssetLoader
 #endif
     }
 
-    public List<Model.TypefaceSpan> FindTypefaces(string text, ShimSkiaSharp.SKPaint paintPreferredTypeface)
+    public List<Model.TypefaceSpan> FindTypefaces(string? text, ShimSkiaSharp.SKPaint paintPreferredTypeface)
     {
         var ret = new List<Model.TypefaceSpan>();
-        if (string.IsNullOrEmpty(text))
-        { return ret; }
+
+        if (text is null || string.IsNullOrEmpty(text))
+        {
+            return ret;
+        }
 
         System.Func<int, SkiaSharp.SKTypeface?> matchCharacter;
 
