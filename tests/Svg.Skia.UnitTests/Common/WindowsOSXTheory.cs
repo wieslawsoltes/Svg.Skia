b0324ffe7c0bf5bd11257f806abe5856282706d3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-28 23:25:54 +0200

    Update WindowsOSXTheory.cs

--- tests/Svg.Skia.UnitTests/Common/WindowsOSXTheory.cs
+++ tests/Svg.Skia.UnitTests/Common/WindowsOSXTheory.cs
@@ -7,9 +7,11 @@ public sealed class WindowsAndOSXTheory : TheoryAttribute
 {
     public WindowsAndOSXTheory(string message = "Windows and OSX only theory")
     {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || !RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ||
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            Skip = message;
+            return;
         }
+        Skip = message;
     }
 }
