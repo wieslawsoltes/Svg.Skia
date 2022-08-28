96f84350d1d916eda3cfe54a772d1bce7c61c779
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-28 22:45:32 +0200

    Move

---
+++ tests/Svg.Skia.UnitTests/SKSvgSettingsTests.cs
@@ -0,0 +1,51 @@
+﻿using SkiaSharp;
+using Svg.Skia.UnitTests.Common;
+using Xunit;
+
+namespace Svg.Skia.UnitTests;
+
+public class SKSvgSettingsTests : SvgUnitTest
+{
+    [Theory]
+    [InlineData("Amiri", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Mplus 1p", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Emoji", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Mono", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Black, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Bold, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Light, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Noto Sans", SKFontStyleWeight.Thin, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright, Skip = "TODO")]
+    [InlineData("Noto Serif", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Sedgwick Ave Display", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Source Sans Pro", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    [InlineData("Yellowtail", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
+    public void Fonts(string fontFamily, SKFontStyleWeight fontWeight, SKFontStyleWidth fontWidth, SKFontStyleSlant fontStyle)
+    {
+        var expectedTypeface = default(SKTypeface);
+
+        if (SKSvgSettings.s_typefaceProviders is { } && SKSvgSettings.s_typefaceProviders.Count > 0)
+        {
+            foreach (var typefaceProviders in SKSvgSettings.s_typefaceProviders)
+            {
+                var skTypeface = typefaceProviders.FromFamilyName(fontFamily, fontWeight, fontWidth, fontStyle);
+                if (skTypeface is { })
+                {
+                    expectedTypeface = skTypeface;
+                    break;
+                }
+            }
+        }
+
+        Assert.NotNull(expectedTypeface);
+
+        if (expectedTypeface is { })
+        {
+            Assert.Equal(fontFamily, expectedTypeface.FamilyName);
+            Assert.Equal((int)fontWeight, expectedTypeface.FontWeight);
+            Assert.Equal((int)fontWidth, expectedTypeface.FontWidth);
+            Assert.Equal(fontStyle, expectedTypeface.FontSlant);
+        }
+    }
+}
--- tests/Svg.Skia.UnitTests/resvgTests.cs
+++ tests/Svg.Skia.UnitTests/resvgTests.cs
@@ -34,49 +34,6 @@ public class resvgTests : SvgUnitTest
         }
     }
 
-    [Theory]
-    [InlineData("Amiri", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Mplus 1p", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Emoji", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Mono", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Black, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Bold, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Light, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Noto Sans", SKFontStyleWeight.Thin, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright, Skip = "TODO")]
-    [InlineData("Noto Serif", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Sedgwick Ave Display", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Source Sans Pro", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    [InlineData("Yellowtail", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright)]
-    public void Fonts(string fontFamily, SKFontStyleWeight fontWeight, SKFontStyleWidth fontWidth, SKFontStyleSlant fontStyle)
-    {
-        var expectedTypeface = default(SKTypeface);
-
-        if (SKSvgSettings.s_typefaceProviders is { } && SKSvgSettings.s_typefaceProviders.Count > 0)
-        {
-            foreach (var typefaceProviders in SKSvgSettings.s_typefaceProviders)
-            {
-                var skTypeface = typefaceProviders.FromFamilyName(fontFamily, fontWeight, fontWidth, fontStyle);
-                if (skTypeface is { })
-                {
-                    expectedTypeface = skTypeface;
-                    break;
-                }
-            }
-        }
-
-        Assert.NotNull(expectedTypeface);
-
-        if (expectedTypeface is { })
-        {
-            Assert.Equal(fontFamily, expectedTypeface.FamilyName);
-            Assert.Equal((int)fontWeight, expectedTypeface.FontWeight);
-            Assert.Equal((int)fontWidth, expectedTypeface.FontWidth);
-            Assert.Equal(fontStyle, expectedTypeface.FontSlant);
-        }
-    }
-
     [Theory(Skip = "TODO")]
     [InlineData("a-alignment-baseline-001", 0.022)]
     public void a_alignment_baseline(string name, double errorThreshold) => TestImpl(name, errorThreshold);
