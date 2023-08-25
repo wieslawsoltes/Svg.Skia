79b3d4c1f6044eeb8f5715af77e0cb5f37032020
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-08-25 12:24:43 +0200

    Handle close after move command

--- src/Svg.Model/SvgExtensions.Pathing.cs
+++ src/Svg.Model/SvgExtensions.Pathing.cs
@@ -264,11 +264,6 @@ public static partial class SvgExtensions
                         return skPath;
                     }
 
-                    if (svgPathSegmentList[i + 1] is SvgClosePathSegment)
-                    {
-                        return skPath;
-                    }
-
                     isEndFigure = true;
                     haveFigure = true;
                     var end = ToAbsolute(svgMoveToSegment.End, svgMoveToSegment.IsRelative, start);
