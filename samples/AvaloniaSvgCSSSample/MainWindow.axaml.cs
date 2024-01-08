6e93bdcea63723418bb45453a3628ceb9f055882
Kalachik Roman <kalachik@eremex.ru>
2024-01-08 23:16:16 +0300

    added example

---
+++ samples/AvaloniaSvgCSSSample/MainWindow.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using System;
+using System.Linq;
+
+namespace AvaloniaSvgCSSSample;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+        ApplyButton.AddHandler(Button.ClickEvent, OnApply);
+    }
+
+    private void OnApply(object sender, EventArgs e) 
+    {
+        this.SetValue(Avalonia.Svg.Skia.Svg.CSSProperty, ".Black { fill: #AAAAFF; }");
+    }
+}
