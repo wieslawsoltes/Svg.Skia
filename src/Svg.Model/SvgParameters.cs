2daaa02a6653068803215b3429bc78f5223eb11f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-01-26 20:01:22 +0100

    Use readonly record struct

--- src/Svg.Model/SvgParameters.cs
+++ src/Svg.Model/SvgParameters.cs
@@ -2,9 +2,4 @@
 
 namespace Svg.Model;
 
-public class SvgParameters(Dictionary<string, string>? entities, string? style)
-{
-    public Dictionary<string, string>? Entities { get; } = entities;
-
-    public string? Style { get; } = style;
-}
+public readonly record struct SvgParameters(Dictionary<string, string>? Entities, string? Style);
