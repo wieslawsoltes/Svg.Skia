6ca397a8c7843837e36cf0ea65070b49c2bea2b9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-03-01 19:06:51 +0100

    Revert "Update supported TargetFramework"
    This reverts commit 4adc9908ccc8a37ab6b0d35f8043d7a69ac63e74.

---
+++ samples/AvaloniaSKPictureImageSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.Skia;
+
+namespace AvaloniaSKPictureImageSample;
+
+internal class Program
+{
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        GC.KeepAlive(typeof(SKPictureImage).Assembly);
+        GC.KeepAlive(typeof(SKPictureControl).Assembly);
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .With(new X11PlatformOptions { })
+            .UseSkia()
+            .LogToTrace();
+    }
+}
