058c1eeef81c23156a4329255a2c21e60893469c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-07-15 20:28:38 +0200

    Expand HitTestEditorSample with svg load/save and property grid

--- samples/HitTestEditorSample/MainWindow.axaml.cs
+++ samples/HitTestEditorSample/MainWindow.axaml.cs
@@ -1,16 +1,20 @@
 using System;
-using System.Drawing;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Svg.Skia;
-using Avalonia.Interactivity;
 using SkiaSharp;
 using Svg;
 using Svg.Skia;
 using Svg.Model.Drawables;
+using Svg.Model.Services;
 
 namespace HitTestEditorSample;
 
@@ -18,7 +22,11 @@ public partial class MainWindow : Window
 {
     private DrawableBase? _selectedDrawable;
     private SvgVisualElement? _selectedElement;
-    private SKColor _boundsColor = SKColors.Red;
+    private SvgDocument? _document;
+
+    private ObservableCollection<PropertyEntry> Properties { get; } = new();
+
+    private readonly SKColor _boundsColor = SKColors.Red;
 
     public MainWindow()
     {
@@ -26,8 +34,74 @@ public partial class MainWindow : Window
 #if DEBUG
         this.AttachDevTools();
 #endif
-        SvgView.Path = "Assets/__tiger.svg";
+        DataContext = this;
+        LoadDocument("Assets/__tiger.svg");
         SvgView.SkSvg!.OnDraw += SvgView_OnDraw;
+    }
+
+    private void LoadDocument(string path)
+    {
+        _document = SvgService.Open(path);
+        if (_document is { })
+        {
+            SvgView.SkSvg!.FromSvgDocument(_document);
+        }
+    }
+
+    private async void OpenMenuItem_Click(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Filters = new()
+            {
+                new FileDialogFilter { Name = "Svg", Extensions = { "svg", "svgz" } },
+                new FileDialogFilter { Name = "All", Extensions = { "*" } }
+            }
+        };
+        var result = await dialog.ShowAsync(this);
+        var file = result?.FirstOrDefault();
+        if (!string.IsNullOrEmpty(file))
+        {
+            LoadDocument(file);
+            Properties.Clear();
+            _selectedDrawable = null;
+            _selectedElement = null;
+            SvgView.InvalidateVisual();
+        }
+    }
+
+    private async void SaveMenuItem_Click(object? sender, RoutedEventArgs e)
+    {
+        if (_document is null)
+            return;
+        var dialog = new SaveFileDialog
+        {
+            Filters = new()
+            {
+                new FileDialogFilter { Name = "Svg", Extensions = { "svg" } },
+                new FileDialogFilter { Name = "All", Extensions = { "*" } }
+            },
+            DefaultExtension = "svg"
+        };
+        var path = await dialog.ShowAsync(this);
+        if (!string.IsNullOrEmpty(path))
+        {
+            _document.Write(path);
+        }
+    }
+
+    private void LoadProperties(SvgVisualElement element)
+    {
+        Properties.Clear();
+        var props = element.GetType()
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(p => p.CanRead && p.CanWrite && p.GetCustomAttribute<SvgAttributeAttribute>() != null);
+        foreach (var prop in props)
+        {
+            var converter = TypeDescriptor.GetConverter(prop.PropertyType);
+            var value = prop.GetValue(element);
+            Properties.Add(new PropertyEntry(prop.GetCustomAttribute<SvgAttributeAttribute>()!.Name, prop, converter.ConvertToInvariantString(value)));
+        }
     }
 
     private void SvgView_OnPointerPressed(object? sender, PointerPressedEventArgs e)
@@ -37,37 +111,26 @@ public partial class MainWindow : Window
         {
             _selectedDrawable = skSvg.HitTestDrawables(pp).FirstOrDefault();
             _selectedElement = skSvg.HitTestElements(pp).OfType<SvgVisualElement>().FirstOrDefault();
+            if (_selectedElement is { })
+            {
+                LoadProperties(_selectedElement);
+            }
+            SvgView.InvalidateVisual();
         }
-        if (_selectedElement is { })
-        {
-            IdTextBox.Text = _selectedElement.ID;
-            FillTextBox.Text = ColorTranslator.ToHtml((_selectedElement.Fill as SvgColourServer)?.Colour ?? System.Drawing.Color.Empty);
-            StrokeTextBox.Text = ColorTranslator.ToHtml((_selectedElement.Stroke as SvgColourServer)?.Colour ?? System.Drawing.Color.Empty);
-        }
-        SvgView.InvalidateVisual();
     }
 
     private void ApplyButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        if (_selectedElement is null)
+        if (_selectedElement is null || _document is null)
             return;
-        _selectedElement.ID = IdTextBox.Text;
-        try
+        foreach (var entry in Properties)
         {
-            var c = ColorTranslator.FromHtml(FillTextBox.Text);
-            _selectedElement.Fill = new SvgColourServer(c);
+            entry.Apply(_selectedElement);
         }
-        catch
-        {
-        }
-        try
-        {
-            var c = ColorTranslator.FromHtml(StrokeTextBox.Text);
-            _selectedElement.Stroke = new SvgColourServer(c);
-        }
-        catch
-        {
-        }
+        SvgView.SkSvg!.FromSvgDocument(_document);
+        Properties.Clear();
+        _selectedDrawable = null;
+        _selectedElement = null;
         SvgView.InvalidateVisual();
     }
 
@@ -83,5 +146,34 @@ public partial class MainWindow : Window
         };
         var rect = SvgView.SkSvg!.SkiaModel.ToSKRect(_selectedDrawable.TransformedBounds);
         e.Canvas.DrawRect(rect, paint);
+    }
+
+    public class PropertyEntry
+    {
+        public string Name { get; }
+        public PropertyInfo Property { get; }
+        public string? Value { get; set; }
+
+        private readonly TypeConverter _converter;
+
+        public PropertyEntry(string name, PropertyInfo property, string? value)
+        {
+            Name = name;
+            Property = property;
+            _converter = TypeDescriptor.GetConverter(property.PropertyType);
+            Value = value;
+        }
+
+        public void Apply(object target)
+        {
+            try
+            {
+                var converted = _converter.ConvertFromInvariantString(Value);
+                Property.SetValue(target, converted);
+            }
+            catch
+            {
+            }
+        }
     }
 }
