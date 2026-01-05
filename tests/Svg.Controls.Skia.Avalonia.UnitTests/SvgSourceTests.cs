3afeafc4f6f90b00dd8ece8dda47ae2b44499280
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-01-05 13:26:11 +0100

    Add tests for Skia Svg cloning and reload

--- tests/Svg.Controls.Skia.Avalonia.UnitTests/SvgSourceTests.cs
+++ tests/Svg.Controls.Skia.Avalonia.UnitTests/SvgSourceTests.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Avalonia.Headless.XUnit;
 using Avalonia.Svg.Skia;
 using ShimSkiaSharp;
+using Svg.Model;
 using Svg.Model.Services;
 using Xunit;
 
@@ -51,5 +55,58 @@ public class SvgSourceTests
 
         Assert.NotNull(source.Picture);
         Assert.NotSame(original, source.Picture);
+    }
+
+    [AvaloniaFact]
+    public void LoadFromSvg_ReLoad_PreservesPicture()
+    {
+        var source = SvgSource.LoadFromSvg(SampleSvg);
+
+        source.ReLoad(new SvgParameters(null, ".Black { fill: #000000; }"));
+
+        Assert.NotNull(source.Svg);
+        Assert.NotNull(source.Picture);
+    }
+
+    [AvaloniaFact]
+    public void Clone_DeepClonesModel()
+    {
+        var source = SvgSource.LoadFromSvg(SampleSvg);
+        var clone = source.Clone();
+
+        Assert.NotSame(source, clone);
+        Assert.NotNull(source.Svg);
+        Assert.NotNull(clone.Svg);
+        Assert.NotSame(source.Svg, clone.Svg);
+        Assert.NotSame(source.Svg?.Model, clone.Svg?.Model);
+        Assert.NotSame(source.Picture, clone.Picture);
+    }
+
+    [AvaloniaFact]
+    public void Dispose_DuringRender_DoesNotDeadlock()
+    {
+        var source = SvgSource.LoadFromSvg(SampleSvg);
+        var beginRender = typeof(SvgSource).GetMethod("BeginRender", BindingFlags.Instance | BindingFlags.NonPublic);
+        var endRender = typeof(SvgSource).GetMethod("EndRender", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        Assert.NotNull(beginRender);
+        Assert.NotNull(endRender);
+
+        var task = Task.Run(() =>
+        {
+            var started = (bool)(beginRender!.Invoke(source, null) ?? false);
+            if (!started)
+            {
+                return false;
+            }
+
+            source.Dispose();
+            endRender!.Invoke(source, null);
+
+            return source.Svg is null && source.Picture is null;
+        });
+
+        Assert.True(task.Wait(TimeSpan.FromSeconds(2)));
+        Assert.True(task.Result);
     }
 }
